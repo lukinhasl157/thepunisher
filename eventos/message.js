8b70135338720d7cae07edacc42b5fbb9e8b5c75
Tsugami <yslan3000@gmail.com>
2018-11-11 16:28:37 -0200

    corrigindo index.js

---
+++ eventos/message.js
@@ -0,0 +1,38 @@
+var recadopraessesfdps = [
+    `<a:mention:500823853971537951> ${message.author}, ta me mencionando pq filho da puta?`, 
+    `<a:mention:500823853971537951> ${message.author}, porra tava quase dormindo e você me menciona?`
+            ];
+
+
+module.exports.run = function(message) {
+    if (message.author.bot || message.channel.type === "dm") return;
+
+    if (message.content.toLowerCase().startsWith(this.botconfig.prefix)) {
+        let args = message.content.split(' ');
+        let nome = args[0].slice(this.botconfig.prefix.length).toLowerCase();
+        let command = this.commands.find((cmd, n) => cmd.aliases.includes(nome) || n === nome);
+
+        if (command) {
+            commandfile.run(bot, message, args);
+        }
+    }
+
+
+    if (message.content.includes(`<@${bot.user.id}>`)) {
+        let msg = recadopraessesfdps[Math.floor(Math.random() * recadopraessesfdps.length)]
+        message.channel.send(msg).then(fdp => fdp.delete(60000));
+    }
+
+
+    if (message.content.toLowerCase().startsWith("flw")) {
+        message.channel.send(`${message.author}, flw viado.`);
+    }
+
+    if (message.guild && !message.member.hasPermission("ADMINISTRATOR")) {
+        if (message.content.includes('https://discord.gg/')) {
+            message.delete();
+            message.channel.send(new Discord.RichEmbed().setDescription(`${message.author} Você não pode divulgar link de servidores aqui! <:blockcustom:500306352695148546>`).setTimestamp().setFooter(`${message.author.tag}`, message.author.displayAvatarURL).setColor("#ff0000"));
+        }
+    }
+
+}
---
+++ eventos/ready.js
@@ -0,0 +1,35 @@
+const botconfig = require("../botconfig.json");
+module.exports.run = function() {
+    // this é o atual Object Global
+    // nesse caso seria o bot pq vc vai chama essa função no evento Ready
+    let status = [
+        `para ${this.users.size} usuários.`,
+        `Está com dúvidas? Digite ${botconfig.prefix}help para receber todas as minhas informações!`,
+        `Não sabe meus comandos? Digite ${botconfig.prefix}!comandos para receber minha lista de comandos!`
+      ];
+      
+        function setStatus() {
+            let randomStatus = status[Math.floor(Math.random() * status.length)];
+            bothist.user.setPresence({game:  {name: randomStatus, type: 'STREAMING', url: 'http://twitch.tv/thedoomsday27'}});
+        }
+      
+    setInterval(() => setStatus(), 15000);
+      
+    console.log("Bot iniciado com sucesso!");
+    console.log('Status carregado com sucesso!');
+
+    let embed = new Discord.RichEmbed()
+        .setColor("#d80abd")
+        .setTitle("Inicialização do bot...")
+        .setThumbnail(this.user.displayAvatarURL)
+        .addField(`Usuários:`, `» ${this.users.size}`)
+        .addField(`Canais:`, `» ${this.channels.size}`)
+        .addField(`Servidores:`, `» ${this.guilds.size}`)
+        .setFooter(`${this.user.tag}`, this.user.displayAvatarURL)
+        .setDescription(`Bot iniciado com sucesso!`)
+        .setTimestamp();
+
+    let channel = bot.channels.find("name", "modlog-the-punisher");
+    if (channel) channel.send(embed);
+      
+}
