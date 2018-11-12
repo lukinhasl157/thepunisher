3391818d3e635a47e7673cca49da85392a05e7ca
Almeeida <42935195+Almeeida@users.noreply.github.com>
2018-11-12 01:57:11 +0000

    to cansado ja

--- commands/clear.js
+++ commands/clear.js
@@ -1,7 +1,6 @@
+const Discord = require("discord.js");
 
-    const Discord = require("discord.js");
-
-    module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
     if (!message.member.hasPermission("MANAGE_MESSAGES"))
     return message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Desculpe, você não tem permissão para executar este comando!`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
@@ -14,6 +13,6 @@
   
       return message.channel.send(new Discord.RichEmbed().setDescription(`${message.author} O chat foi limpo com sucesso <a:sucessogif:499614074129350666>`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#07ed66")).then(msg => msg.delete(10000));
   }
-  module.exports.help = {
+module.exports.help = {
     name: "clear"
-  }
+}
