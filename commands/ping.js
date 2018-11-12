bcdb756929201aba3695ddb3100c9c9b62625215
Almeeida <42935195+Almeeida@users.noreply.github.com>
2018-11-12 01:54:02 +0000

    tirei os espaços

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 
-        module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
-        const m = await message.channel.send("<:think:499596179290456074> Ping?"); 
-        m.edit(new Discord.RichEmbed().setDescription(`:ping_pong: Pong! A latência do bot é **${m.createdTimestamp - message.createdTimestamp}ms.** A lantência da API é **${Math.round(bot.ping)}ms.** <:wifi2:501137858250145810>`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#07ed66"));
-      } 
+const m = await message.channel.send("<:think:499596179290456074> Ping?"); 
+    m.edit(new Discord.RichEmbed().setDescription(`:ping_pong: Pong! A latência do bot é **${m.createdTimestamp - message.createdTimestamp}ms.** A lantência da API é **${Math.round(bot.ping)}ms.** <:wifi2:501137858250145810>`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#07ed66"));
+    } 
       
-      module.exports.help = {
-        name: "ping"
+module.exports.help = {
+   name: "ping"
 }
