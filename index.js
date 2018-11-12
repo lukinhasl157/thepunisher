f1b56a84c8a651ae03d571a4e235625bbf104024
Tsugami <yslan3000@gmail.com>
2018-11-12 00:03:08 -0200

    gg lukas

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const config = require("./config.json");
 // BOT 
 const bot = new Discord.Client();
 
+// GG LUKAS
 // Definando variaveis dentro do bot
 Object.defineProperties(bot, {
   "commands": {
