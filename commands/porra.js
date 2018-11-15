00980707f7ad1bc711dc6445b6b39edbdf8f21fe
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-15 21:11:14 -0200

    teste

--- commands/porra.js
+++ commands/porra.js
@@ -1,6 +1,10 @@
 
 module.exports.run = async (bot, message, args) => { 
 
+    if (args[0] === Number) {
+        return message.channel.send("O argumento precisa ser um número.")
+    }
+
     if (args[0] / 2 && parseInt(args[0])) {
         return message.channel.send("true")
 
