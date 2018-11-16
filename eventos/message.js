dce1f83a1fe3d7ecc4ef205c0afb61e48fe77d8c
Tsugami <yslan3000@gmail.com>
2018-11-16 00:22:59 -0200

    Update message.js

--- eventos/message.js
+++ eventos/message.js
@@ -1,5 +1,5 @@
 const { RichEmbed } = require('discord.js');
-module.exports.run = function(message) {
+module.exports.run = async function(message) {
     if (message.author.bot || message.channel.type === "dm")
         return;
 
@@ -7,14 +7,16 @@ module.exports.run = function(message) {
 
         Object.defineProperty(message, 'prefix', { value: this.config.prefix });
 
-        let args = message.content.split(' ');
-        let nome = args[0].slice(this.config.prefix.length).toLowerCase();
+        let args = message.content.slice(this.config.prefix.length).split(' ');
+        let nome = args.shift().toLowerCase();
         let command = this.commands.find((cmd, n) => (cmd.aliases && cmd.aliases.includes(nome)) || n === nome);
         
         if (command) {
 
-            if (command.usersCooldown.has(message.author.id)) 
-                return message.channel.send("Desse jeito você vai fuder meu processador, aguarde ``3s.``").then(m => m.delete(60000));           
+            if (command.usersCooldown.has(message.author.id)) {
+                let m = await message.channel.send("Desse jeito você vai fuder meu processador, aguarde ``3s.``");
+                return m.delete(60000);
+            }           
             
             Object.defineProperty(message, 'command', { value: command });
 
@@ -33,7 +35,7 @@ module.exports.run = function(message) {
         `<a:mention:500823853971537951> Olá, ${message.author} meu desenvolvedor tem demencia e ainda não fez essa parte.`, 
     ];
 
-    
+
     if (!message.command) {
         if (message.content.includes(`<@${this.user.id}>`)) {
             let msg = recadopraessesfdps[Math.floor(Math.random() * recadopraessesfdps.length)]
