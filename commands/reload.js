b42ad3ee14dd00fe50cad8297b44c5847e33db53
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-16 16:01:31 -0200

    fiz merda

--- commands/reload.js
+++ commands/reload.js
@@ -4,8 +4,8 @@
   module.exports.run = async (bot, message, args) => {
 
   if (message.author.id !== '289209067963154433') return message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Este comando é exclusivo para desenvolvedores.`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000")).then(message.delete());
-  let args = args.slice(0).join(' ');
-  if (!args) return message.channel.send(new Discord.RichEmbed().setDescription(`Por favor, digite o nome do comando que deseja dar reload.`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
+  let commands = args.slice(0).join(' ');
+  if (!commands) return message.channel.send(new Discord.RichEmbed().setDescription(`Por favor, digite o nome do comando que deseja dar reload.`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
   
     try {
       delete require.cache[require.resolve(`./${args[0]}.js`)];
