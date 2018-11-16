69f921e5cb080f8d86b1f615659d0c375bf3f1ed
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-16 01:50:55 -0200

    pronto tirei sas porra

--- commands/ascii.js
+++ commands/ascii.js
@@ -23,18 +23,3 @@ module.exports = {
 }
 
 
-if (comando === "registro") {
-
-const a = new Discord.RichEmbed()
-.setTitle("ReactRole")
-.setDescription("Clique na reação :white_small_square:  para conseguir a role")
-let msg = message.channel.send(a)
-    msg.react(":white_small_square:")
-
-    bot.on('messageReactionAdd', (reaction, user) => {
-    if(reaction.emoji.name == "nomedoemoji" && user.id !== bot.user.id){
-    message.member.addRole("id")
-    }
-})
-}
-
