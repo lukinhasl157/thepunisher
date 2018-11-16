b3906378bd4d6cb102a9f86655f1cbcffc822429
Tsugami <yslan3000@gmail.com>
2018-11-16 00:18:53 -0200

    Update message.js

--- eventos/message.js
+++ eventos/message.js
@@ -1,28 +1,39 @@
 const { RichEmbed } = require('discord.js');
 module.exports.run = function(message) {
-    if (message.author.bot || message.channel.type === "dm") return;
+    if (message.author.bot || message.channel.type === "dm")
+        return;
 
     if (message.content.toLowerCase().startsWith(this.config.prefix)) {
+
         Object.defineProperty(message, 'prefix', { value: this.config.prefix });
+
         let args = message.content.split(' ');
         let nome = args[0].slice(this.config.prefix.length).toLowerCase();
         let command = this.commands.find((cmd, n) => (cmd.aliases && cmd.aliases.includes(nome)) || n === nome);
         
         if (command) {
-            if (command.usersCooldown.has(message.author.id)) return message.channel.send("Desse jeito você vai fuder meu processador, aguarde ``3s.``").then(msg => msg.delete(60000));           
-            Object.defineProperty(message, 'command', { value: command });    
+
+            if (command.usersCooldown.has(message.author.id)) 
+                return message.channel.send("Desse jeito você vai fuder meu processador, aguarde ``3s.``").then(m => m.delete(60000));           
+            
+            Object.defineProperty(message, 'command', { value: command });
+
             command.run(this, message, args.slice(1));
             command.usersCooldown.add(message.author.id);
+
             setTimeout(function() {
                 command.usersCooldown.delete(message.author.id);
             }, command.cooldown);
+
         }
+
     }
-const recadopraessesfdps = [
+
+    const recadopraessesfdps = [
         `<a:mention:500823853971537951> Olá, ${message.author} meu desenvolvedor tem demencia e ainda não fez essa parte.`, 
     ];
 
-    // açoes que não pode ser executadas junto com algum comando
+    
     if (!message.command) {
         if (message.content.includes(`<@${this.user.id}>`)) {
             let msg = recadopraessesfdps[Math.floor(Math.random() * recadopraessesfdps.length)]
