ee2c3e40095c400e782b1154b0715fe500fe1955
Almeeida <42935195+Almeeida@users.noreply.github.com>
2018-11-18 19:27:10 +0000

    Updated chatlock.js
    pq só quem tem adm pode bloquear o chat? wtf

--- commands/chatlock.js
+++ commands/chatlock.js
@@ -3,22 +3,15 @@ const Discord = require("discord.js");
 module.exports = {
     run: (bot, message, args) => {
 
-
-     if (!message.member.hasPermission("ADMINISTRATOR")) {
-     return message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Desculpe, você não tem permissão para executar este comando!`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
-     
-     }
+if (!message.member.hasPermission("MANAGE_CHANNELS")) {
+  return message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Desculpe, você não tem permissão para executar este comando!`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));    
+}
   var lock1 = message.guild.roles.find(r => r.name === "@everyone");
   message.channel.overwritePermissions(lock1, {
-     SEND_MESSAGES: false
-
-     
-   })
+     SEND_MESSAGES: false     
+})
    message.channel.send(new Discord.RichEmbed().setDescription(`» O canal ${message.channel} foi **DESATIVADO.** :lock:`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
-
-
 },
-
     aliases: ["chlock", "lock"],
     category: "Moderação",
     description: "Desativar o chat."
