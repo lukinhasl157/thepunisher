18bcb91d1de04e27f8e54ae25fb409f28a156b15
Almeeida <42935195+Almeeida@users.noreply.github.com>
2018-11-18 19:08:32 +0000

    testando umas merdas

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -20,7 +20,7 @@ let roles = message.guild.roles.map(a => a.name).join(", ").replace('@everyone, 
       .setColor("#FF0000")
       .addField(':crown: » Dono:', `<@${message.guild.owner.id}>`)
       .addField(':calendar: » Servidor criado em:', moment(message.guild.createdAt).format('LLLL'))
-      .addField("<:world:500147421641310229> » Região:", message.guild.region)
+      .addField("<:world:500147421641310229> » Região:", moment(message.guild.region))
       .addField(":file_cabinet: » ID do servidor:", message.guild.id)
       .addField(`<:user:500109138953633792> » Total de membros: [${totalmembros}]`, `<:online:513046143572377601> » Online: ${online}\n<:ausente:513046210672590848> » Ausente: ${ausente}\n <:ocupado:513046183699283968> » Ocupado: ${ocupado}\n <:offline:513046162731827203> » Offline: ${offline}\n:robot: » Bots: ${bots}`)
       .addField(`» Total de canais: [${canaistexto+canaisvoz}]`, `Texto: ${canaistexto}\n Voz: ${canaisvoz}`)
