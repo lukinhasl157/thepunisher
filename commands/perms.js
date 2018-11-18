1531cdd53991c8c79cb79327964e0de462287d47
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-18 20:06:57 -0200

    Update perms.js

--- commands/perms.js
+++ commands/perms.js
@@ -5,14 +5,14 @@ exports.run = async (bot, message, args) => {
 
     let user = message.mentions.members.first() || message.member;
     let string = ''
-    message.channel.permissionsFor(user).toArray().map(p => string += `${p.charAt(0) + p.toLowerCase().replace(/_/g, ' ').slice(1).replace(`vad`, `VAD`)}**,  **`)
+    message.channel.permissionsFor(user).toArray().map(p => string += `${p.charAt(0) + p.toLowerCase().replace(/_/g, ' ').slice(1).replace(`vad`, `VAD`)}`)
     let finalStr = string 
     let embed = new Discord.RichEmbed()
     .setDescription(`:pasta: Permissões de ${message.author.username} em ${message.guild.name}\nLista de Permissões:\n${finalStr}`)
-    .setColor('#81BEF7')
-       .setTimestamp(new Date())
-             .setFooter(message.author.tag, message.author.avatarURL)
-           .setThumbnail(message.author.avatarURL)
+    .setColor('#ff0000')
+    .setTimestamp(new Date())
+    .setFooter(message.author.tag, message.author.avatarURL)
+    .setThumbnail(message.author.avatarURL)
     message.channel.send(embed);
 }
 
