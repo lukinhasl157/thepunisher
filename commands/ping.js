96fffd3593d53d85f90f427b27c53fba0e390957
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-19 15:05:47 -0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 module.exports = {
 	run: async function(bot, message, args) {
 
-  let msg = await message.channel.send("Calculando a latência...");
+  let msg = await mesage.channel.send("Calculando a latência...");
 
    	setTimeout(() => {
   	msg.edit(`:ping_pong: Pong! **${message.author.username}**, a latência do bot é \`\`${msg.createdTimestamp - message.createdTimestamp}ms.\`\` A lantência da API é \`\`${Math.round(bot.ping)}ms.\`\` <:wifi2:501137858250145810>`);
-	}, 3000)
+	}, 2000).catch(e => message.channel.send(`Deu merda quando tentei executar o comando ping. Erro: ${e}`);
 
     
    	return this.name;
