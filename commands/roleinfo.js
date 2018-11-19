1d00d373cec652c42b6d50bb9a188d6d1b040417
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-19 14:38:07 -0200

    arrumando roleinfo e serverinfo

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -2,27 +2,29 @@ const Discord = require("discord.js");
 const moment = require("moment");
 moment.locale('pt-BR');
 
-module.exports.run = async (bot, message, args) => {
-
+module.exports ={
+    run: (bot, message, args) => {
 
     let role = message.guild.roles.find(r => r.name === `${args.join(" ")}`);
 
-    if (!role) return message.channel.send(new Discord.RichEmbed().setDescription(`Por favor, digite o nome do cargo que deseja ver as informações.`).setFooter(`Comando solicitado por ${message.author.tag}`, message.author.displayAvatarURL).setColor("#ff0000").setTimestamp());
+    if (!role) return message.channel.send(`**${message.author.username}** Por favor, digite o nome do cargo que deseja ver as informações.`);
 
-    const embed = new Discord.RichEmbed()
-    .setAuthor(`Informações do cargo ${role.name}`)
+    let embed = new Discord.RichEmbed()
+    .setAuthor(`» Informações do cargo ${role.name}`)
     .setColor("RANDOM")
-    .addField("Nome do cargo:", `${role.name}`)
-    .addField("ID do cargo:", `${role.id}`)
-    .addField("Cor:", `${role.hexColor}`)
-    .addField("Cargo criado em:", `${moment(role.createdAt).format('LLL')}`)
-    .addField('Permissões:', `\`\`\`css\n${Object.entries(role.serialize()).filter(([,has]) => has).map(([perm]) => perm).join(", ")}\`\`\``, false)
+    .addField("» Nome do cargo:", role.name)
+    .addField("» ID do cargo:", role.id)
+    .addField("» Posição do cargo:", role.position)
+    .addField("» Cor:", role.hexColor)
+    .addField("» Cargo criado em:", `${moment(role.createdAt).format('LLL')}`)
+    .addField('» Permissões:', `\`\`\`css\n${Object.entries(role.serialize()).filter(([,has]) => has).map(([perm]) => perm).join(", ")}\`\`\``, false)
     .setFooter(`Comando solicitado por ${message.author.tag}`, message.author.displayAvatarURL)
+    
     message.channel.send(embed);
     
-    }
-    
-    module.exports.help = {
-        name: "roleinfo"
-    }
+},
+    aliases: ["ri", "cargoinfo"],
+    category: "Moderação",
+    description: "Mostar as informações do cargo."
+}
     
