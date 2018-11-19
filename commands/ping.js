c88d107e79f6d0ee539e74ce316f85b341b489da
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-19 13:38:03 -0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,7 +7,7 @@ module.exports = {
 
    setTimeout(() => {
 msg.edit(`:ping_pong: Pong! **${message.author.username}**, a latência do bot é \`\`${msg.createdTimestamp - message.createdTimestamp}ms.\`\` A lantência da API é \`\`${Math.round(bot.ping)}ms.\`\` <:wifi2:501137858250145810>`);
-}, 4000)
+}, 2000)
     
    	return this.name;
 
