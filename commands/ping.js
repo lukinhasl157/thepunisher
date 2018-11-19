745b1a56b7302f049b912f5128802b139a3d6298
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-19 14:58:58 -0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,12 +4,10 @@ module.exports = {
 	run: async function(bot, message, args) {
 
   let msg = await message.channel.send("Calculando a latência...");
-  	message.channel.startTyping();
 
    	setTimeout(() => {
   	msg.edit(`:ping_pong: Pong! **${message.author.username}**, a latência do bot é \`\`${msg.createdTimestamp - message.createdTimestamp}ms.\`\` A lantência da API é \`\`${Math.round(bot.ping)}ms.\`\` <:wifi2:501137858250145810>`);
-   	message.channel.stopTyping();
-	}, 2000)
+	}, 3000)
 
     
    	return this.name;
