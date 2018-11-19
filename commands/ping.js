34d5270fb7fca74bd4b43e63ee18786f5a2fb22d
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-19 14:55:43 -0200

    porra tenho cancer

--- commands/ping.js
+++ commands/ping.js
@@ -4,11 +4,11 @@ module.exports = {
 	run: async function(bot, message, args) {
 
   let msg = await message.channel.send("Calculando a latência...");
-  	channel.startTyping();
+  	message.channel.startTyping();
 
    	setTimeout(() => {
   	msg.edit(`:ping_pong: Pong! **${message.author.username}**, a latência do bot é \`\`${msg.createdTimestamp - message.createdTimestamp}ms.\`\` A lantência da API é \`\`${Math.round(bot.ping)}ms.\`\` <:wifi2:501137858250145810>`);
-   	message.chnnel.stopTyping();
+   	message.channel.stopTyping();
 	}, 2000)
 
     
