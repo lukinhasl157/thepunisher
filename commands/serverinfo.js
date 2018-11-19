20612673f7380e3a6c9f664e959e380fbf2f4c11
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-18 22:18:33 -0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -14,6 +14,8 @@ module.exports = {
     let canaistexto = message.guild.channels.filter(a => a.type === "text").size;
     let canaisvoz = message.guild.channels.filter(a => a.type === "voice").size;
     let roles = message.guild.roles.map(a => a.name).join(", ").replace('@everyone, ', '');
+    let canalAfk = message.guild.afkChannel.replace("null", "Nenhum canal afk.");
+    let canalAfkTempo = message.guild.afkChannel.replace("null", "Nenhum tempo.");
 
         let embed = new Discord.RichEmbed()
         .setAuthor(`» ${message.guild.name}`, `${message.guild.iconURL}`)
@@ -24,7 +26,7 @@ module.exports = {
         .addField(":file_cabinet: » ID do servidor:", message.guild.id)
         .addField(`<:user:500109138953633792> » Total de membros: [${totalmembros}]`, `<:online:513046143572377601> Online: ${online}\n<:ausente:513046210672590848> Ausente: ${ausente}\n <:ocupado:513046183699283968> Ocupado: ${ocupado}\n <:offline:513046162731827203> Offline: ${offline}`)
         .addField(":robot: » Bots:", `${bots}`)
-        .addField(`» Canal afk:\n${message.guild.afkChannel}`, `» Tempo afk:\n${message.guild.afkTimeout}`, true)
+        .addField("» Canal afk:                » Tempo afk:", `${canalAfk}                ${canalAfkTempo}`, true)
         .addField(`<:pensenisso:513802114657419274> » Total de emojis:`, message.guild.emojis.size)
         .addField(`» Total de canais: [${canaistexto+canaisvoz}]`, `Texto: ${canaistexto}\n Voz: ${canaisvoz}`)
         .addField(`» Total de cargos: [${message.guild.roles.size}]`, `\`\`\`\n${roles}\`\`\``)
