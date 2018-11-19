5d7588c2e4a2ab7f09e47de18135e9d2ec9d0e10
Almeeida <42935195+Almeeida@users.noreply.github.com>
2018-11-19 00:41:21 +0000

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -16,20 +16,21 @@ module.exports = {
     let roles = message.guild.roles.map(a => a.name).join(", ").replace('@everyone, ', '');
     let canalAfk = message.guild.afkChannel;
     let canalAfkTempo = message.guild.afkChannelTimeout;
+    let inline = '1';
 
         let embed = new Discord.RichEmbed()
         .setAuthor(`» ${message.guild.name}`, `${message.guild.iconURL}`)
         .setColor("#FF0000")
-        .addField(':crown: » Dono:', `<@${message.guild.ownerID}>`)
-        .addField(':calendar: » Servidor criado em:', moment(message.guild.createdAt).format('LLLL'))
-        .addField("<:world:500147421641310229> » Região:", message.guild.region)
-        .addField(":file_cabinet: » ID do servidor:", message.guild.id)
-        .addField(`<:user:500109138953633792> » Total de membros: [${totalmembros}]`, `<:online:513046143572377601> Online: ${online}\n<:ausente:513046210672590848> Ausente: ${ausente}\n <:ocupado:513046183699283968> Ocupado: ${ocupado}\n <:offline:513046162731827203> Offline: ${offline}`)
-        .addField(":robot: » Bots:", `${bots}`)
-        .addField("» Canal afk:                » Tempo afk:", `${canalAfk}                ${canalAfkTempo}`, true)
-        .addField(`<:pensenisso:513802114657419274> » Total de emojis:`, message.guild.emojis.size)
-        .addField(`» Total de canais: [${canaistexto+canaisvoz}]`, `Texto: ${canaistexto}\n Voz: ${canaisvoz}`)
-        .addField(`» Total de cargos: [${message.guild.roles.size}]`, `\`\`\`\n${roles}\`\`\``)
+        .addField(':crown: » Dono:', `<@${message.guild.ownerID}>`, inline)
+        .addField(':calendar: » Servidor criado em:', moment(message.guild.createdAt).format('LLLL'), inline)
+        .addField("<:world:500147421641310229> » Região:", message.guild.region, inline)
+        .addField(":file_cabinet: » ID do servidor:", message.guild.id, inline)
+        .addField(`<:user:500109138953633792> » Total de membros: [${totalmembros}]`, `<:online:513046143572377601> Online: ${online}\n<:ausente:513046210672590848> Ausente: ${ausente}\n <:ocupado:513046183699283968> Ocupado: ${ocupado}\n <:offline:513046162731827203> Offline: ${offline}`, inline)
+        .addField(":robot: » Bots:", `${bots}`, inline)
+        .addField("» Canal afk:                » Tempo afk:", `${canalAfk}                ${canalAfkTempo}`, true, inline)
+        .addField(`<:pensenisso:513802114657419274> » Total de emojis:`, message.guild.emojis.size, inline)
+        .addField(`» Total de canais: [${canaistexto+canaisvoz}]`, `Texto: ${canaistexto}\n Voz: ${canaisvoz}`, inline)
+        .addField(`» Total de cargos: [${message.guild.roles.size}]`, `\`\`\`\n${roles}\`\`\``, inline)
         .setThumbnail(message.guild.iconURL)
         .setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.avatarURL)
         message.channel.send(embed);
