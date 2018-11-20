9b32dbc32ff2bf32dcc391672bd021f1403db01c
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-20 15:40:03 -0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -8,8 +8,7 @@ module.exports = {
   let msg = await message.channel.send("Calculando a latência...");
 
    	setTimeout(() => {
-  	msg.edit(new Discord.RichEmbed().setDescription(`:ping_pong: Pong! **${message.author.username}**, a latência do bot é \`\`${msg.createdTimestamp - message.createdTimestamp}ms.\`\` A lantência da API é \`\`${Math.round(bot.ping)}ms.\`\` <:wifi2:501137858250145810>`).setImage("https://cdn.discordapp.com/attachments/507638961447895050/510135398501187604/pingpong.gif").setColor("#ff0000").setTimestamp(new Date()).setFooter(message.author.displayAvatarURL, message.author.tag));
-    message.channel.send("https://cdn.discordapp.com/attachments/425865939691765760/514251837549969420/pingpong.gif")
+  	msg.edit(new Discord.RichEmbed().setDescription(`:ping_pong: Pong! **${message.author.username}**, a latência do bot é \`\`${msg.createdTimestamp - message.createdTimestamp}ms.\`\` A lantência da API é \`\`${Math.round(bot.ping)}ms.\`\` <:wifi2:501137858250145810>`).setImage("https://cdn.discordapp.com/attachments/507638961447895050/510135398501187604/pingpong.gif").setColor("#ff0000").setTimestamp(new Date()).setFooter(message.author.tag, message.author.displayAvatarURL));
 	}, 2000)
 
    } catch(e) {
