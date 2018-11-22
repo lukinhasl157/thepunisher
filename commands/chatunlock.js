3bc89b5e76e0f7fc5e9b7bd6a53c8a4d1523973d
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-21 23:07:22 -0200

    ADW

--- commands/chatunlock.js
+++ commands/chatunlock.js
@@ -1,16 +1,15 @@
-const Discord = require("discord.js");
 
 module.exports = {
-  run: (bot, message, args) => {
+    run: (bot, message, args) => {
 
 if (!message.member.hasPermission("MANAGE_CHANNELS")) {
-  return message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Desculpe, você não tem permissão para executar este comando!`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000")); 
+  return message.channel.sendmessage.channel.send(`**${message.author.username}** | Desculpe, você não permissão para executar este comando! Permissão requirida: **MANAGE_CHANNELS**`);
 }
-  var lock = message.guild.roles.find(r => r.name === "@everyone");
- message.channel.overwritePermissions(lock, {
-     SEND_MESSAGES: true
+  var everyone = message.guild.roles.find(r => r.name === "@everyone");
+  message.channel.overwritePermissions(everyone, {
+     SEND_MESSAGES: true  
 })
-   message.channel.send(new Discord.RichEmbed().setDescription(`» O canal ${message.channel} foi **ATIVADO.** :unlock:`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
+   message.channel.send(`» O canal ${message.channel} foi **REATIVADO.** :unlock: por **${message.author.username}**`);
 },
   aliases: ["chunlock", "unlock"],
   category: "Moderação",
