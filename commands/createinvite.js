9e923b2d8ba865264f824edd8d2351bdd5a08020
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-23 17:48:11 -0200

    awd

--- commands/createinvite.js
+++ commands/createinvite.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 
     module.exports = {
         run: async function (bot, message, args) {
-        
-
 
     try {
     const invite = await message.channel.createInvite({maxAge: 0});
@@ -12,8 +10,6 @@ const Discord = require('discord.js');
         } catch (err) {
             message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Desculpe, você não tem permissão para criar convites!`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
     }
-
-    return this.name;
 
 },
 
