5830bc1016751d310547b9d09c96edc04fc75a0a
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-23 21:36:14 -0200

    fdp n ta funfando com emoji custom ou seu sou burro msm

--- commands/teste.js
+++ commands/teste.js
@@ -21,11 +21,11 @@ module.exports = {
       		return message.channel.send(`» **${message.author.username}** | Desculpe, eu não tenho as permissões necessárias para banir este usuário!`);
       	}
 
-		let msg = await message.channel.send(`» **${message.author.username}** | Você tem certeza de banir o usuário ${member} pelo motivo: "${reason}" ? Se sim, clique no emoji <:correto:505155063963058187> para bani-lo. Se não clique no emoji <:negado:505155029636874250> para cancelar esta ação.`);
+		let msg = await message.channel.send(`» **${message.author.username}** | Você tem certeza de banir o usuário ${member} pelo motivo: **${reason}** ? Se **SIM**, clique no emoji <:correto:505155063963058187> para bani-lo. Se **NÃO** clique no emoji <:negado:505155029636874250> para cancelar esta ação.`);
 			await msg.react(":correto:505155063963058187");
             await msg.react(":negado:505155029636874250");
 
-            	const filter = (reaction, member) => reaction.emoji.id === "<:correto:505155063963058187>" && member.id === message.author.id;
+            	const filter = (reaction, member) => reaction.emoji.name === "<:correto:505155063963058187>" && member.id === message.author.id;
             	const collector = msg.createReactionCollector(filter, {time: 60000});
 
             		collector.on("collect", r => {
@@ -35,7 +35,7 @@ module.exports = {
             			msg.channel.send(`» O usuário **${member.user.username}** ID: \`\`${member.user.id}\`\`| Foi banido com sucesso. :correto:505155063963058187`);
             		})
 
-            	const filter2 = (reaction, member) => reaction.emoji.id === "<:negado:505155029636874250>" && member.id === message.author.id;
+            	const filter2 = (reaction, member) => reaction.emoji.name === "<:negado:505155029636874250>" && member.id === message.author.id;
             	const collector2 = msg.createReactionCollector(filter2, {time: 60000});
 
             		collector2.on("collect", r => {
