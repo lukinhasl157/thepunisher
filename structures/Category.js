e044a44f07d3068b16d7c7534979581c667de3a3
Tsugami <yslan3000@gmail.com>
2018-11-23 17:58:49 -0200

    Update Category.js

--- structures/Category.js
+++ structures/Category.js
@@ -1,6 +1,9 @@
+const { Collection } = require('discord.js');
+const fs = require('fs');
 class Category {
-    constructor(name, data) {
+    constructor(client, name, data) {
         this.name = name;
+        this.client = client;
 
         this.description = '';
         this.emoji = '';
@@ -8,8 +11,24 @@ class Category {
         this.dir = '';
 
         for (const key in data)
-            if (!this[key]) this[key] = data[key]; 
+            if (!this[key]) this[key] = data[key];
+            
+        this.directory = `${__dirname}/../${this.dir}`;
+
+        this.commands = new Collection();
     }
+
+    initialize() {
+        fs.readdirSync(this.directory).forEach((file) => {
+            if (file.endsWith('.js')) {
+                let Command = require(this.directory + '/' + file);
+                let command = new Command(this.client);
+
+                this.commands.set(file.replace('.js'), command);
+            }
+        });
+    }
+    
 }
 
 module.exports = Category;
