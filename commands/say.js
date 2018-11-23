e5a81a42931356513dac57ecd13eaacf23662305
Tsugami <yslan3000@gmail.com>
2018-11-23 17:26:21 -0200

    teste

--- commands/say.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{});
-    message.channel.send(sayMessage);
-  }
-module.exports.help = {
-    name: "say"
-}
