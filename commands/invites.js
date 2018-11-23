e6d8fec7a83827c26a10175312a42a0821f33366
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-23 15:31:27 -0200

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
     module.exports = {
-        run: (bot, message, args) => {
+        run: async function (bot, message, args) {
 
      let user = message.mentions.users.first();
 
