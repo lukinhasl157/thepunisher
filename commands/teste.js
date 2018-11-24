4aeff48be08534a9f8e859d19dfe11b76150cb1d
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-23 22:21:18 -0200

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -4,24 +4,20 @@ module.exports = {
 
 		try {
 
-		var member = message.mentions.members.first() || message.guild.members.get(args[0]);
-		var reason = args.slice(1).join(" ");
 
-		if (!message.member.hasPermission("BAN_MEMBERS")) {
+		if (!message.member.hasPermission("BAN_MEMBERS"))
 			return message.channel.send(`» **${message.author.username}** | Desculpe você não tem permissão para executar este comando! Permissão requirida: **BAN_MEMBERS**.`);
-		}
 		
-		if (!member) {
+		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+		if (!member)
 			return message.channel.send(`» **${message.author.username}** | Por favor, insira o id ou mencione o usuário que deseja banir.`);
-		}
 
-		if (!reason) {
+		let reason = args.slice(1).join(" ");
+		if (!reason) 
 			return message.channel.send(`» **${message.author.username}** | Por favor, insira um motivo para banir este usuário.`);
-		}
 
-		if (!member.bannable) {
+		if (!member.bannable) 
       		return message.channel.send(`» **${message.author.username}** | Desculpe, eu não tenho as permissões necessárias para banir este usuário!`);
-      	}
 
 		let msg = await message.channel.send(`» **${message.author.username}** | Você tem certeza de banir o usuário ${member} pelo motivo: **${reason}** ? Se **SIM**, clique no emoji ✅ para bani-lo. Se **NÃO** clique no emoji ❌ para cancelar esta ação.`);
 			await msg.react("✅");
