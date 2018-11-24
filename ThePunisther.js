541d59722064d5007960acc633f7638773bc1d1c
Tsugami <yslan3000@gmail.com>
2018-11-24 01:13:20 -0200

    adicionando use JsDOC
    facilita a vida dos outros desenvolvedores

--- ThePunisther.js
+++ ThePunisther.js
@@ -1,7 +1,19 @@
 const { Client, Collection } = require('discord.js');
 const { Category } = require('./structures/');
 
+/**
+ * Client princial do The Punisther
+ * @extends Discord.Client
+ * @prop {Object} config arquivo config.json
+ * @prop {Collection<Category>} categories As categorias do bot
+ */
 class ThePunisther extends Client {
+    /**
+     *  Cria o Bot
+     * @param {Object} config arquivo config.json
+     * @param {Object} categories arquivo categories.json
+     * @param {Object} options Opções requisita por Discord.js
+     */
     constructor(config = {}, categories = {}, options = {}) {
         super(options);
          
--- structures/Category.js
+++ structures/Category.js
@@ -1,6 +1,26 @@
 const { Collection } = require('discord.js');
+
 const fs = require('fs');
+
+/**
+ * Estrutura das categorias do bot
+ * @prop {String} id abreviação do nome da categoria
+ * @prop {ThePunisther} client client do bot
+ * @prop {String} name nome da categoria
+ * @prop {String} description descrição que será usado no comando help
+ * @prop {String} emoji Emoji que será usado no comando help
+ * @prop {Number} color Cor dos embeds da categoria
+ * @prop {String} dir pasta dos comandos
+ * @prop {String} directory diretório da pasta dos comandos
+ * @prop {Collection<Command>} commands os comandos da categoria
+ */
 class Category {
+    /**
+     * 
+     * @param {ThePunisther} client client do bot 
+     * @param {String} id  abreviação do nome da categoria
+     * @param {Object} data propriedades da categorias
+     */
     constructor(client, id, data) {
         this.id = id;
         this.client = client;
@@ -21,6 +41,7 @@ class Category {
         this.initialize();
     }
 
+    // adiciona os comandos da pasta no this.commands
     initialize() {
         fs.readdirSync(this.directory).forEach((file) => {
             if (file.endsWith('.js')) {
