6cfc750e48c6b768784d1dadbcac7a39621def5d
Tsugami <yslan3000@gmail.com>
2018-11-24 15:17:47 -0200

    adicionando JsDoc no Embed.js

--- structures/Embed.js
+++ structures/Embed.js
@@ -2,6 +2,11 @@ const { MessageEmbed } = require("discord.js");
 const moment = require("moment");
 moment.locale('pt-BR');
 
+/**
+ * @property {Message} _message mensagem de quem usou o comando
+ * @property {Command} _command comando que está sendo usado
+ * @property {ThePunisther} _bot cliente do bot
+ */
 class Embed extends MessageEmbed {
     constructor(message, data) {
         super(data);
@@ -16,11 +21,19 @@ class Embed extends MessageEmbed {
         this.footerUserUsed();
     }
 
+    /**
+     * seta o footer do embed com o nome e o avatar do usuario e a data que ele usou
+     * @returns {Embed}
+     */
     footerUserUsed() {
         this.setFooter(`${this._message.author.tag} • ${moment().format('ll')}`, this._message.author.avatarURL());
         return this;
     }
 
+    /** 
+     * @param {Command} command comando que será colocado no footer
+     * @returns {Embed}
+    */
     footerHelp(command) {
         command = command || this._command;
         if (command) this.setFooter(`utilize ${process.env.prefix}help ${command.name} pra mais informações`);
