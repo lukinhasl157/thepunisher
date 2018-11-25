49997e5bb0c93f5be14a1c4af1406070cfec1f03
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-24 22:48:49 -0200

    Update ready.js

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,4 +1,3 @@
-const { RichEmbed } = require('discord.js');
 module.exports.run = function() {
     let status = [
         `para ${this.users.size} usuários.`,
@@ -12,21 +11,11 @@ module.exports.run = function() {
       
     setInterval(() => setStatus(), 15000);
       
+    this.channels.get("515877820358983693").setName(`» Servidores: [${this.guilds.size}]`);
+    this.channels.get("516051234168242179").setName(`» Usuários: [${this.users.size}]`);
+    this.channels.get("516051255014195201").setName(`» Canais: [${this.channels.size}]`);
+
     console.log("Bot iniciado com sucesso!");
     console.log('Status carregado com sucesso!');
-
-    let embed = new RichEmbed()
-        .setColor("#d80abd")
-        .setTitle("Inicialização do bot...")
-        .setThumbnail(this.user.displayAvatarURL)
-        .addField(`Usuários:`, `» ${this.users.size}`)
-        .addField(`Canais:`, `» ${this.channels.size}`)
-        .addField(`Servidores:`, `» ${this.guilds.size}`)
-        .setFooter(`${this.user.tag}`, this.user.displayAvatarURL)
-        .setDescription(`Bot iniciado com sucesso!`)
-        .setTimestamp();
-
-    let channel = this.channels.find(c => c.name === "modlog-the-punisher");
-    if (channel) channel.send(embed);
       
 }
