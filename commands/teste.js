74e187640956c1707cf8386748a44089c0e17eac
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-29 17:48:48 -0200

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -1,5 +1,5 @@
 module.exports = {
-	run: (message, args) => {
+	run: (bot, message, args) => {
 
 		let number = args.join(" ");
 		let number2 = (number >= 12) ? "Este número é maior" : "Este número é menor";
