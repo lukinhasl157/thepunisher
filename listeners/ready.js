42b8251d34e01ebb1b072b43a84f5135293aebae
Tsugami <yslan3000@gmail.com>
2018-11-29 21:25:37 -0200

    github desktop fdp

--- listeners/ready.js
+++ listeners/ready.js
@@ -1,6 +1,6 @@
 function Ready() {
     Object.defineProperty(this, 'defaultChannels', {value: {}});
-
+    
     for (const name in this.config.channelIDs)
         this.defaultChannels[name] = this.channels.get(this.config.channelIDs[name]) || null;
 
