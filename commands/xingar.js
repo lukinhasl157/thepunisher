9e1cd336f0417085a631da759bd506011647aa52
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-30 18:41:41 -0200

    Update xingar.js

--- commands/xingar.js
+++ commands/xingar.js
@@ -4,7 +4,7 @@ module.exports = {
 		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
 
 		if (!member)
-			return message.channel.send(`**${message.author.username}**, | Por favor, insira o id ou mencione o usuário que deseja xingar.`);
+			return message.channel.send(`**${message.author.username}**, | Por favor, insira o id ou mencione o usuário que deseja xingar. Após \`\`30s\`\` mensagem será apagada.`);
 
 		message.channel.send(`**${message.author.username}** | Como deseja xingar o usuário **${member.user.tag}** ?`).then(msg => {
 			msg.delete(30000);
@@ -15,8 +15,8 @@ module.exports = {
 				collector.on("collect", async m => {
 
 					message.channel.send(`**${message.author.username}**, seu xingamento enviado com sucesso.`);
-					await message.channel.send(`${member}, o usuário **${message.author.username}**, Xingou você de ${m.content}`);
-					await msg.stop(message.author.id);
+					await message.channel.send(`${member}, o usuário **${message.author.username}**, xingou você de: "${m.content}"`);
+					await m.stop(message.author.id);
 
 				})
 
