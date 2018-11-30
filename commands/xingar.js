019f6f892a80f2d31b18bc80ef74911ea6e7e5e0
Lukinhas <lukinhasbisquao@gmail.com>
2018-11-30 18:05:09 -0200

    awd

---
+++ commands/xingar.js
@@ -0,0 +1,29 @@
+module.exports = {
+	run: (bot, message, args) => {
+
+		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+
+		if (!member)
+			return message.channel.send(`**${message.author.username}**, | Por favor, insira o id ou mencione o usuário que deseja xingar.`);
+
+		message.channel.send(`**${message.author.username}** | Como deseja xingar o usuário **${member.user.tag}** ?`).then(msg => {
+			msg.delete(30000);
+
+			const filter = m => m.author.id === message.author.id;
+			const collector = msg.channel.createMessageCollector(filter, {time: 30000});
+
+				collector.on("collect", m => {
+
+					message.channel.send(`O usuário **${message.author.username}** | Xingou você de ${m.content}`);
+
+				})
+
+		})
+
+		
+	}
+}
+
+
+
+
