9879e8d713e3879e4870ca6bfd4ce36b46b0cae6
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-02 22:18:52 -0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,11 +1,12 @@
   const Discord = require("discord.js");
 
-  module.exrpots = {
+  module.exports = {
     run: async function (bot, message, args) {
 
-    if (!message.member.hasPermission("KICK_MEMBERS"))
-        return message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Desculpe, você não tem permissão para executar este comando!`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
+      try {
 
+    if (!message.member.hasPermission("KICK_MEMBERS"))
+        return message.channel.send(`**${message.author.username}** | Desculpe, você não permissão para executar este comando! Permissão requirida: **KICK_MEMBERS**.`
       let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if (!member)
         return message.channel.send(new Discord.RichEmbed().setDescription(`${message.author} Por favor, mencione o membro que deseja kickar.`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
@@ -14,12 +15,16 @@
         return message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Desculpe, você não tem as permissões necessárias para expulsar este usuário`).setColor("#ff0000"));
   
       let reason = args.join(' ');
-    if (!reason) reason = "Nenhuma motivo foi citado.";
+    if (!reason)
+      return message.channel.send(`**${message.author.username}** | Por favor, diga um motivo para expulsar este usuário.`)
         
       await member.kick(reason)
-        .catch(error => message.channel.send(new Discord.RichEmbed().setDescription(`Desculpe ${message.author} não consegui expulsar esse este usuário devido o erro: ${error}`).setColor("#ff0000")));
         message.channel.send(new Discord.RichEmbed().setDescription(`O usuário ${member.user.tag} foi kickado  por ${message.author}`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setThumbnail(member.user.avatarURL).addField(`Id do usuário:` , `» ${member.user.id}`).addField(`Motivo:` , `» ${reason}`).setTimestamp().setColor("#ff0000"));
     
+      } catch(e) {
+        message.channel.send(`**${message.author.usarname}** | Deu merda aqui: ${e}`)
+      }
+
       },
         aliases: ["expulsar", "chutar"],
         category: "Moderação",
