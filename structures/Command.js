00d7aa720317866698431cbd9a872ffe52765ae9
Tsugami <yslan3000@gmail.com>
2018-12-03 15:36:51 -0200

    Update Command.js

--- structures/Command.js
+++ structures/Command.js
@@ -52,7 +52,7 @@ class Command {
     checkError(message, args) {
         let errors = this.errorMessages;
         
-        if (this.developerOnlys && !this.bot.guild.member(message.author).roles.get(this.process.env.ROLE_DEV_ID))
+        if (this.developerOnlys && !this.bot.guild.member(message.author).roles.get(process.env.ROLE_DEV_ID))
             return true;
 
         if (args.length === 0 && this.argsRequired)
