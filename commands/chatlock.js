6916fbe467a95af9999e9e361015bd94c476a749
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-02 22:06:52 -0200

    chat lock e chat unlock

--- commands/chatlock.js
+++ commands/chatlock.js
@@ -2,17 +2,24 @@
 module.exports = {
     run: (bot, message, args) => {
 
+    	try {
+
 if (!message.member.hasPermission("MANAGE_CHANNELS")) {
-  return message.channel.sendmessage.channel.send(`**${message.author.username}** | Desculpe, você não permissão para executar este comando! Permissão requirida: **MANAGE_CHANNELS**`);
+  return message.channel.send(`**${message.author.username}** | Desculpe, você não permissão para executar este comando! Permissão requirida: **MANAGE_CHANNELS**`);
 }
   var everyone = message.guild.roles.find(r => r.name === "@everyone");
   message.channel.overwritePermissions(everyone, {
      SEND_MESSAGES: false     
 })
    message.channel.send(`» O canal ${message.channel} foi **DESATIVADO.** :lock: por **${message.author.username}**`);
+
+} catch (e) {
+	message.channel.send(`**${message.author.usarname}** | Erro: Eu não tenho a permissão de **GERENCIAR_CANAIS**.`)
+}
    
 },
     aliases: ["chlock", "lock"],
     category: "Moderação",
     description: "Desativar o chat."
 }
+
