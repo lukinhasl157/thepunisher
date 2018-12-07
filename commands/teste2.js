885c461702a59720d5c41c849a015edbca58c9ad
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-07 18:39:11 -0200

    Update teste2.js

--- commands/teste2.js
+++ commands/teste2.js
@@ -3,35 +3,35 @@ const Discord = require("discord.js");
 module.exports = {
     run: async function(bot, message, args) {
 
-let embed = new Discord.RichEmbed()
-.setDescription("Clique no emoji para criar o canal")
-await message.channel.send(embed).then(async msg => {
-await msg.react("✅");
+    let embed = new Discord.RichEmbed()
+    .setDescription("Clique no emoji para criar o canal")
+    await message.channel.send(embed).then(async msg => {
+    await msg.react("✅");
 
-const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === message.author.id;
-const collector = msg.createReactionCollector(filter, {time: 30000});
+        const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === message.author.id;
+        const collector = msg.createReactionCollector(filter, {time: 30000});
 
-collector.on("collect", async r => {
+    collector.on("collect", async r => {
 
-  let channel = message.guild.channels.find(ch => ch.name === "nomedocanal");
-  let category = message.guild.channels.find(c => c.name === "nomedacategoria");
+    let channel = message.guild.channels.find(ch => ch.name === "nomedocanal");
+    let category = message.guild.channels.find(c => c.name === "nomedacategoria");
 
-if (category || channel) {
-    return message.channel.send("Este canal já existe");
-    r.remove(message.author.id);
-}
+            if (category || channel) {
+                r.remove(message.author.id);
+                return message.channel.send("Este canal já existe");
+            }
 
-if (!channel || !category || category.type !== "category") {
-category = await message.guild.createChannel("nomedacategoria", "category");
-channel = await message.guild.createChannel("nome do canal", "text");
-await channel.setParent(category.id);
-await message.channel.send("Canal criado com sucesso");
-r.remove(message.author.id);
-}
+            if (!channel || !category || category.type !== "category") {
+                r.remove(message.author.id);
+                category = await message.guild.createChannel("nomedacategoria", "category");
+                channel = await message.guild.createChannel("nome do canal", "text");
+                await channel.setParent(category.id);
+                await message.channel.send("Canal criado com sucesso");
+            }
 
-})
+        });
 
-})
+        })
 
-}
+    }
 }
