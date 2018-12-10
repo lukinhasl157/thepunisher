cb726182e03616a61ccff00a082b32a7ef5cadbf
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-09 22:22:12 -0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,17 +5,6 @@ module.exports = {
 	try {
 
 		//gif ban the punisher https://media.giphy.com/media/1Xe14KOTgtL86EGBXU/giphy.gif
-		
-		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-		if (!member)
-			return message.channel.send(`» **${message.author.username}** | Por favor, insira o id ou mencione o usuário que deseja banir.`);
-
-		let reason = args.slice(1).join(" ");
-		if (!reason) 
-			return message.channel.send(`» **${message.author.username}** | Por favor, insira um motivo para banir este usuário.`);
-
-		if (!member.bannable) 
-      		return message.channel.send(`» **${message.author.username}** | Desculpe, eu não tenho as permissões necessárias para banir este usuário!`);
 
 		let msg = await message.channel.send(`» **${message.author.username}** | Você tem certeza de banir o usuário ${member} pelo motivo: **${reason}** ? Se **SIM**, clique no emoji <:correto:505155063963058187> para bani-lo. Se **NÃO** clique no emoji <:negado:505155029636874250> para cancelar esta ação.`);
             await msg.react(":correto:505155063963058187");
@@ -25,11 +14,24 @@ module.exports = {
     				const collector = msg.createReactionCollector(filter, {time: 60000 });
 
             		collector.on("collect", r => {
+
+                        r.remove(message.author.id);
+                        msg.delete();
+
+                        let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+                         if (!member)
+                            return message.channel.send(`» **${message.author.username}** | Por favor, insira o id ou mencione o usuário que deseja banir.`);
+
+                        let reason = args.slice(1).join(" ");
+                        if (!reason) 
+                            return message.channel.send(`» **${message.author.username}** | Por favor, insira um motivo para banir este usuário.`);
+
+                        if (!member.bannable) 
+                            return message.channel.send(`» **${message.author.username}** | Desculpe, eu não tenho as permissões necessárias para banir este usuário!`);
+
                         if (!message.member.hasPermission("BAN_MEMBERS"))
-                            msg.delete();
                             return message.channel.send(`» **${message.author.username}** | Desculpe você não tem permissão para executar este comando! Permissão requirida: **BAN_MEMBERS**.`);
-            			r.remove(message.author.id);
-            			msg.delete();
+
             			switch (r._emoji.name) {
             				case "correto":
             					member.send(`» **${member.user.username}** | Você foi banido por **${message.author.username}**. » Motivo: ${reason}.`);
