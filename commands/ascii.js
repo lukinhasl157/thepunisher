92eb640f7a82f8d113f2cdd055f5f0606ec3809d
Tsugami <yslan3000@gmail.com>
2018-12-12 15:54:20 -0200

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -2,25 +2,24 @@ const ascii = require("ascii-art");
 
 module.exports = {
     run: async (bot, message, args) => {
+        let text = args.join(' ')
 
-        let texto = args.join(" ");
+        if (!text) {
+            return message.channel.send(`» **${message.author.username}** | Os argumentos não podem passar de  **15** caracteres.`)
+        }
 
-            ascii.font(texto, 'Doom', function(rendered){
+        ascii.font(text, 'Doom', function (rendered) {
+            rendered = rendered.trimRight()
 
-            rendered = rendered.trimRight();
-
-            if (texto.length >= 15) {
-                return message.channel.send(`» **${message.author.username}** | Os argumentos não podem passar de  **15** caracteres.`);
+            if (text.length >= 15) {
+                return message.channel.send(`» **${message.author.username}** | Os argumentos não podem passar de  **15** caracteres.`)
             }
 
             message.channel.send(rendered, {
                 code: 'md'
-            });
-        });
-            
+            })
+        })  
     },
-    category: "Entreterimento",
-    description: "Formatar um texto em ascii"
+    category: 'Entreterimento',
+    description: 'Formatar um texto em ascii'
 }
-
-
