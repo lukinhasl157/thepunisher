b37e1eb7adf7eac322a4c8e5451788faa5b398f8
Tsugami <yslan3000@gmail.com>
2018-12-12 15:57:47 -0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,23 +1,21 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js')
 
 module.exports = {
-  run: (bot, message, args) => {
+    run: (bot, message, [id]) => {
+        let member = message.guild.member(message.mentions.users.first() || bot.users.get(id) || message.author)
+        let mAvatar = member.user.displayAvatarURL || member.displayAvatarURL
 
-    let member = message.guild.member(message.mentions.users.first() || bot.users.get(args[0]) || message.author);
-    let mAvatar = member.user.avatarURL;
+        let embed = new Discord.RichEmbed()
+            .setColor(member.displayColor)
+            .setAuthor(`» Avatar do usuário: ${member.user.tag}`, member.user.displayAvatarURL)
+            .setDescription(`Clique [aqui](${mAvatar}) para fazer o download da imagem.`)
+            .setImage(mAvatar)
+            .setFooter(`${message.guild.name}`, message.guild.iconURL)
+            .setTimestamp()
 
-      let embed = new Discord.RichEmbed()
-      .setColor(member.displayColor)
-      .setAuthor(`» Avatar do usuário: ${member.user.tag}`, member.user.displayAvatarURL)
-      .setDescription(`Clique [aqui](${mAvatar}) para fazer o download da imagem.`)
-      .setImage(mAvatar)
-      .setFooter(`${message.guild.name}`, message.guild.iconURL)
-      .setTimestamp()
-      message.channel.send(embed);
-  
+        message.channel.send(embed)
     },
-      
-      aliases: ["av"],
-      category: "Utilidades",
-      description: "Mostrar a foto de perfil do usuário."
-    }
+    aliases: ['av'],
+    category: 'Utilidades',
+    description: 'Mostrar a foto de perfil do usuário.'
+}
