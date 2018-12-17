6ee13b0c9c4e70bbfe11cbdfe054e03929fe3693
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-17 17:51:56 -0200

    Update fdp.js

--- commands/fdp.js
+++ commands/fdp.js
@@ -1,10 +1,10 @@
 module.exports = {
 	run: (bot, message, args) => {
 
-		let member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.guild.members.get(args.join(" "));
+		let member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.guild.members.find(m => m.user.username === `${args.join(" ")}`);
 
 		if (args.length === 0) {
-			return message.channel.send(`» **${message.author.username}** | Por favor, insira o id, nome ou mencione o usuário que deseja banir.`);
+			return message.channel.send(`» **${message.author.username}** | Por favor, insira o id, nome ou mencione o usuário que deseja xingar.`);
 		}
 
 		if (member) {
