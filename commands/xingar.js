fd40cdab3cd80aea4de955e87367f0d152622a1a
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-17 18:38:22 -0200

    Update xingar.js

--- commands/xingar.js
+++ commands/xingar.js
@@ -8,7 +8,7 @@ module.exports = {
 		if (!member)
 			return message.channel.send(`**${message.author.username}**, | Por favor, insira o id ou mencione o usuário que deseja xingar.`);
 
-			message.channel.send(`**${message.author.username}** | Como deseja xingar o usuário **${member.user.tag}** ? (Após \`\`30s\`\` esta mensagem será apagada.)`).then(msg => {
+			message.author.send(`**${message.author.username}** | Como deseja xingar o usuário **${member.user.tag}** ? (Após \`\`30s\`\` esta mensagem será apagada.)`).then(msg => {
 			msg.delete(30000);
 
 			const filter = m => m.author.id === message.author.id;
@@ -16,14 +16,14 @@ module.exports = {
 
 				collector.on("collect", async m => {
 
-					message.channel.send(`**${message.author.username}**, seu xingamento enviado com sucesso.`);
+					message.author.send(`**${message.author.username}**, seu foi xingamento enviado com sucesso.`);
 					let porra = await message.channel.send(`${member}, o usuário **${message.author.username}**, xingou você de: "${m.content}". Caso queira xingar o usuário de volta clique no emoji "<:velhodoinfarto:517877465750700033>"`);
 					await porra.react(":velhodoinfarto:517877465750700033");
 					await collector.stop(message.author.id);
 
 				})
 
-			const filter2 = (r, u) => r.emoji.id === "517877465750700033" && u.id === message.author.id;
+			const filter2 = (r, u) => r.emoji.id === "517877465750700033" && u.id === member.id;
 			const collector2 = msg.createReactionCollector(filter2, {time: 60000});
 
 				collector2.on("collect", r => {
