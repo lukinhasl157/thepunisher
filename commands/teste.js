4a47f27170d8b5ed7afddff18746550242edca50
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-26 12:06:03 -0200

    funciona porra

--- commands/teste.js
+++ commands/teste.js
@@ -4,17 +4,17 @@ module.exports = {
 		let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 		let numberRandom = numbers[Math.floor(Math.random() * numbers.length)];
 
-		message.channel.send("Evento loteria iniciado!");
+		await message.channel.send("Evento loteria iniciado!");
 
 		try {
 
-		const filter = msg => msg.content.startsWith(numberRandom);
-		const collector = channel.awaitMessages(filter, { max: 1, time: 300 * 1000, errors: ["time"] });
+		const filter = message.content.startsWith(numberRandom);
+		const collector = await channel.awaitMessages(filter, { max: 1, time: 300000, errors: ["time"] });
 
-			message.channel.send(`${collector.first()} venceu o evento loteria. O número correto era ${collector.first().content}`);
+			return message.channel.send(`${collector.first()} venceu o evento loteria. O número correto era ${numberRandom}`);
 
-		} catch (e) {
-			message.channel.send(`O evento loteria acabou, nenhum usuário venceu. O número correto era ${numberRandom}`);
+		} catch (time) {
+			return message.channel.send(`O evento loteria acabou, nenhum usuário venceu. O número correto era ${numberRandom}`);
 		}
 
 	}
