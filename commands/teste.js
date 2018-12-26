a5c34118a1d680421b7631403f326f5131a6b999
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-26 15:33:25 -0200

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -6,19 +6,16 @@ module.exports = {
 
 		message.channel.send("Evento loteria iniciado!");
 
-		const filter = msg => msg.content.startsWith(numberRandom);
-		const collector = message.channel.createMessageCollector(filter, { time: 300000 });
+		try {
 
-			collector.on("collect", msg => {
+			const filter = msg => msg.content.startsWith(numberRandom);
+			const collector = message.channel.awaitMessages(filter, { max: 1, time: 300000, erros: ["time"] });
 
 				message.channel.send(`${msg.content.first()} venceu o evento loteria. O número correto era ${numberRandom}`);
 
-			});
-			
-			collector.on("end", collected => {
-
-				message.channel.send(`O evento loteria acabou, nenhum usuário venceu. O número correto era ${numberRandom}`);
-			})
+		} catch(e) {
+			return message.channel.send(`O evento loteria acabou, nenhum usuário venceu. O número correto era ${numberRandom}`);
+		}
 
 	}
 }
