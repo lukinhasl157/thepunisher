6fa6574841224228b1dd40860c1840b6b5074d1c
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-26 12:17:23 -0200

    Update teste.js

--- commands/teste.js
+++ commands/teste.js
@@ -1,15 +1,15 @@
 module.exports = {
-	run: async function (bot, message, args) {
+	run: (bot, message, args) => {
 
 		let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 		let numberRandom = numbers[Math.floor(Math.random() * numbers.length)];
 
-		await message.channel.send("Evento loteria iniciado!");
+		message.channel.send("Evento loteria iniciado!");
 
 		try {
 
 		const filter = message.content.startsWith(numberRandom);
-		const collector = await channel.awaitMessages(filter, { max: 1, time: 300000, errors: ["time"] });
+		const collector = message.channel.awaitMessages(filter, { max: 1, time: 300000, errors: ["time"] });
 
 			return message.channel.send(`${collector.first()} venceu o evento loteria. O número correto era ${numberRandom}`);
 
