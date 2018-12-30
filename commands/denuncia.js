1884a1de1a80f7bdedf6ff12fd12c5cb5a473231
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-30 01:37:56 -0200

    Update denuncia.js

--- commands/denuncia.js
+++ commands/denuncia.js
@@ -16,6 +16,7 @@ module.exports = {
         const collector = message.channel.createMessageCollector(filter, {time: 300 * 1000, max: 1});
 
         collector.on("collect", m => {
+            collector.stop();
             message.author.send(`2º Passo: Qual o motivo que deseja denunciar o usuário **${m.content.toLowerCase().includes()}**`);
         });
 
@@ -23,7 +24,7 @@ module.exports = {
         const collector2 = message.channel.createMessageCollector(filter2, {time: 300 * 1000, max: 1});
 
         collector2.on("collect", m2 => {
-            m2.stop();
+            collector2.stop();
             message.author.send(`3º Passo: Tem certeza que deseja denunciar o usuário **${m1.content.toLowerCase().includes()}** pelo motivo \`${m2.content.toLowerCase().includes()}\`?\nSe deseja enviar a denuncia digite **sim**, caso não queira enviar a denuncia digite **não**`);
         });
 
@@ -33,7 +34,7 @@ module.exports = {
         collector3.on("collect", async m3 => {
 
             if (m3.content.toLowerCase().includes("sim")) {
-                m3.stop();
+                collector3.stop();
                 let channel = message.guild.channel.find(ch => ch.name === "Denuncias");
                 if (!channel) {
                     channel = await message.guild.createChannel("Denuncias", "text", [{
@@ -47,7 +48,7 @@ module.exports = {
                     channel.send(`teste\nUsuário: ${m1.content.toLowerCase().includes()}\nMotivo: ${m2.content.toLowerCase().includes()}`);
                 };
             } else if (m3.content.toLowerCase().includes("não" || "nao")) {
-                m3.stop();
+                collector3.stop();
                 message.author.send("Sua denuncia foi cancelada com sucesso.");
             } else {
                 message.author.send("Este argumento não é uma resposta válida, digite **SIM** para enviar a denuncia ou digite **NÃO** para cancelar a denuncia.");
