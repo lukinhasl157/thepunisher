5e459869443619196021f531eface3a37cccef9a
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-30 02:04:08 -0200

    Update denuncia.js

--- commands/denuncia.js
+++ commands/denuncia.js
@@ -19,12 +19,16 @@ module.exports = {
             let msg1 = m.content;
             message.author.send(`2º Passo: Qual o motivo que deseja denunciar o usuário **${msg1.toLowerCase().includes()}**`);
 
+        });
+
         const filter2 = m2 => m2.author.id = message.author.id;
         const collector2 = message.channel.createMessageCollector(filter2, {time: 300 * 1000, max: 1});
 
         collector2.on("collect", m2 => {
-            let mg2 = m2.content;
+            let msg2 = m2.content;
             message.author.send(`3º Passo: Tem certeza que deseja denunciar o usuário **${msg1.toLowerCase().includes()}** pelo motivo \`${msg2.toLowerCase().includes()}\`?\nSe deseja enviar a denuncia digite **sim**, caso não queira enviar a denuncia digite **não**`);
+
+        });
 
         const filter3 = m3 => m3.author.id = message.author.id;       
         const collector3 = message.channel.createMessageCollector(filter3, {time: 300 * 1000, max: 1});
@@ -32,7 +36,7 @@ module.exports = {
         collector3.on("collect", async m3 => {
 
             let msg3 = m3.content;
-            if (m3.toLowerCase().includes("sim")) {
+            if (msg3.toLowerCase().includes("sim")) {
                 let channel = message.guild.channel.find(ch => ch.name === "Denuncias");
                 if (!channel) {
                     channel = await message.guild.createChannel("Denuncias", "text", [{
@@ -45,14 +49,12 @@ module.exports = {
                 } else {
                     channel.send(`teste\nUsuário: ${msg1.toLowerCase().includes()}\nMotivo: ${msg2.toLowerCase().includes()}`);
                 };
-            } else if (m3.toLowerCase().includes("não" || "nao")) {
+            } else if (msg3.toLowerCase().includes("não" || "nao")) {
                 message.author.send("Sua denuncia foi cancelada com sucesso.");
             } else {
                 message.author.send("Este argumento não é uma resposta válida, digite **SIM** para enviar a denuncia ou digite **NÃO** para cancelar a denuncia.");
             };
 
-                });
-            });            
         });
     },
     aliases: ["denunciar", "report", "repotar", "denuncia"],
