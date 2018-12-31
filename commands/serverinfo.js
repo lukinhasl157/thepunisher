6bb7cb1281651c7b8be47e8d46ee4b36bc198444
Lukinhas <lukinhasbisquao@gmail.com>
2018-12-31 04:15:37 -0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -27,9 +27,9 @@ module.exports = {
         .addField(":zzz: » Canal afk", `${message.guild.afkChannel ? message.guild.afkChannel.name : "Nenhum canal afk."}`, inline) 
         .addField("<:world:500147421641310229> » Região:", message.guild.region.toString().replace("brazil", ":flag_br: Brasil"), inline)
         .addField("<:fast:500147391945768981> » Tempo afk", `${message.guild.afkTimeout} segundos`, inline)
-        .addField(`<:user:500109138953633792> » Membros: [${totalmembros}]`, `<:online:513046143572377601> Online: ${online}\n<:ausente:513046210672590848> Ausente: ${ausente}\n <:ocupado:513046183699283968> Ocupado: ${ocupado}\n <:offline:513046162731827203> Offline: ${offline}\n <:bot:437248340724416514> Bots: ${bots}`, inline)
+        .addField(`<:user:500109138953633792> » Membros: [${totalmembros}]`, `<:online:529179015865434132> Online: ${online}\n<:ausente:513046210672590848> Ausente: ${ausente}\n <:ocupado:529178886647578626> Ocupado: ${ocupado}\n <:offline:529178943882788866> Offline: ${offline}\n <:bot:529180656090087424> Bots: ${bots}`, inline)
         .addField(`🛡 » Nivel de verificação:`, message.guild.verificationLevel.toString().replace("0", "Nenhum (sem restrições)").replace("1", "Baixo: Precisa tem um e-mail verificado em sua conta do Discord.").replace("2", "Médio: Também precisa ser registrado no Discord por pelo menos 5 minutos.").replace("3", "Alto: Também precisa ser um membro deste servidor por pelo menos 10 minutos.").replace("4", "Muito alto: Precisa ter um telefone verificado em sua conta do Discord."), inline)
-        .addField(`:beginner: » Total de cargos: [${message.guild.roles.size}]`, inline)
+        .addField(`:beginner: » Total de cargos: [${message.guild.roles.size}]`)
         .setThumbnail(message.guild.iconURL)
         .setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.avatarURL)
 
