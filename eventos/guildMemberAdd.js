c044117f7c1ad8fb8a3cb62e18414c8a41cdd2e5
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-05 16:41:40 -0200

    arrumando ou tentando arrumar os eventos de entrada/saida

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -10,9 +10,9 @@ module.exports = {
 		try {
 
 		let channel = member.guild.channels.find(ch => ch.name === "🎉bem-vindos");
-		let category = member.guild.channels.find(c => c.name === "👾entrada/saida");
+		let category = member.guild.channels.find(ch => ch.name === "👾entrada/saida");
 
-		let embed = new Discord.RichEmbed()
+		const embed = new Discord.RichEmbed()
       	.setColor("#3fdb20")
       	.setThumbnail(member.user.avatarURL)
       	.setDescription(`${member}, bem vindo(a)! ao servidor :tada:`)
@@ -20,37 +20,29 @@ module.exports = {
       	.setTimestamp(new Date())
       	.setFooter(member.guild.name, member.guild.iconURL)
     	
-			if (!channel || !category || category.type !== "category") {
-
+			if (!channel || !category || category.type !== "category" || category.name === "👾entrada/saida", "category") {
 				category = await member.guild.createChannel("👾entrada/saida", "category");
 				channel = await member.guild.createChannel("🎉bem-vindos", "text" [{
 					id: member.guild.id,
 					deny: ["SEND_MESSAGES"],
 					allow: ["ADD_REACTIONS", "VIEW_CHANNEL"]
 				}])
-
 				await channel.setParent(category.id);
-				await member.send(embed)
+				await member.send(embed);
 				let carai = await channel.send(embed);
 				await carai.react("🎉");
 				await carai.react(":bemvindo:523560019841515520");
 
-			} else {
-
-				member.send(embed);
-				channel.send(embed).then(async porra => {
-				await porra.react("🎉");
-				await porra.react(":bemvindo:523560019841515520");
-
-				})
+			} else if (channel || category || category.type === "category" || category.name === "👾entrada/saida") {
+				await channel.setParent(category.id);
+				await member.send(embed);
+				let m = await channel.send(embed);
+				await m.react("🎉");
+				await m.react(":bemvindo:523560019841515520");
 			}
-
 		} catch(e) {
-
-			let channel = this.channels.find(ch => ch.name === "❌logs-de-erros-the-punisher");
-			
+			const channel = this.channels.find(ch => ch.name === "❌logs-de-erros-the-punisher");
 			channel.send(`Ocorreu um erro no evento **guildMemberAdd** | Servidor ${member.guild.name}. Erro: ${e}`)
-		
 		}	
 
 	}
--- eventos/guildMemberRemove.js
+++ eventos/guildMemberRemove.js
@@ -10,9 +10,9 @@ module.exports = {
 		try {
 
 		let channel = member.guild.channels.find(ch => ch.name === "🎊saida");
-		let category = member.guild.channels.find(c => c.name === "👾entrada/saida");
+		let category = member.guild.channels.find(ch => ch.name === "👾entrada/saida");
 
-		let embed = new Discord.RichEmbed()
+		const embed = new Discord.RichEmbed()
       	.setColor("#3fdb20")
       	.setThumbnail(member.user.avatarURL)
       	.setDescription(`${member}, saiu do servidor.`)
@@ -21,33 +21,25 @@ module.exports = {
       	.setFooter(member.guild.name, member.guild.iconURL)
     	
 			if (!channel || !category || category.type !== "category") {
-
 				category = await member.guild.createChannel("👾entrada/saida", "category");
 				channel = await member.guild.createChannel("🎊saida", "text" [{
 					id: member.guild.id,
 					deny: ["SEND_MESSAGES"],
 					allow: ["VIEW_CHANNEL", "ADD_REACTIONS"]
-				}])
+				}]);
 				await channel.setParent(category.id);
 				await member.send(embed);
 				let carai = await channel.send(embed);
 				await carai.react("🎉");
 				await carai.react(":bemvindo:523560019841515520");
 			} else if (channel || category || category.type === "category" || category.name === "👾entrada/saida") {
-
 				channel.setParent(category.id);
 				channel.send(embed);
-			} else {
-
-				channel.send(embed);
 			}
-
-		} catch(e) {
-
-			let channel = this.channels.find(ch => ch.name === "❌logs-de-erros-the-punisher");
 			
+		} catch(e) {
+			const channel = this.channels.find(ch => ch.name === "❌logs-de-erros-the-punisher");
 			channel.send(`Ocorreu um erro no evento **guildMemberAdd** | Servidor ${member.guild.name}. Erro: ${e}`)
-		
 		}	
 
 	}
