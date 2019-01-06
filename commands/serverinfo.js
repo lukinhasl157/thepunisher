24f09d887853c4fe20d4a234434c483dbc98570a
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-05 22:58:48 -0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -29,7 +29,7 @@ module.exports = {
         .addField("<:fast:500147391945768981> » Tempo afk", `${message.guild.afkTimeout} segundos`, inline)
         .addField(`<:user:500109138953633792> » Membros: [${totalmembros}]`, `<:online:529179015865434132> Online: ${online}\n<:ausente:513046210672590848> Ausente: ${ausente}\n <:ocupado:529178886647578626> Ocupado: ${ocupado}\n <:offline:529178943882788866> Offline: ${offline}\n <:bot:529180656090087424> Bots: ${bots}`, inline)
         .addField(`🛡 » Nivel de verificação:`, message.guild.verificationLevel.toString().replace("0", "Nenhum (sem restrições)").replace("1", "Baixo: Precisa tem um e-mail verificado em sua conta do Discord.").replace("2", "Médio: Também precisa ser registrado no Discord por pelo menos 5 minutos.").replace("3", "Alto: Também precisa ser um membro deste servidor por pelo menos 10 minutos.").replace("4", "Muito alto: Precisa ter um telefone verificado em sua conta do Discord."), inline)
-        .addField(`:beginner: » Total de cargos: [${message.guild.roles.size}]`, `Para ver todos os cargos do servidor clique no emoji <a:arrowRight:531248395411521566> para voltar a página inicial clique no emoji <a:arrowLeft:531247468260622353>`)
+        .addField(`:beginner: » Total de cargos: [${message.guild.roles.size}]`, `Para ver todos os cargos do servidor clique no emoji <a:arrowRight:531248395411521566>\nPara voltar a página inicial clique no emoji <a:arrowLeft:531247468260622353>`)
         .setThumbnail(message.guild.iconURL)
         .setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.avatarURL)
         let msg = await message.channel.send(embed);
