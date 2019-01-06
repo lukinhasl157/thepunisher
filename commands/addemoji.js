3bcac86b6a96a34103b34232e3917fc874a1c3ec
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-06 05:06:23 -0200

    Update addemoji.js

--- commands/addemoji.js
+++ commands/addemoji.js
@@ -1,14 +1,13 @@
 module.exports = {
 	run: (bot, message, args) => {
 
-		if (args.join(" ").slice(0).length === 0) {
+		if (args[0].length === 0) {
 			return message.channel.send(`**${message.author.username}** | Desculpe o link deste emoji é inválido.`);
-		} else if (args.join(" ").slice(1).length === 0) {
+		} else if (args.join(" ").length === 0) {
 			return message.channel.send(`**${message.author.username}** | Por favor, insira um nome para este emoji.`);
 		} else {
-			message.guild.emojis.create(args.join("").slice(0), args.join("").slice(1));
-			const emoji = message.guild.emojis.find(e => e.name === args.join("").slice(1));
-			message.channel.send(`${emoji.name} criado com sucesso!\n ${emoji.url}`);
+			message.guild.emojis.create(`${args[0]}`, `${args.join(" ")}`);
+			message.channel.send(`Emoji **${args.join(" ")}** criado com sucesso!`);
 		}
 	}
 }
