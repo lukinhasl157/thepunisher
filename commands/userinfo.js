b3ed1d1716f22f141ed816cf845814e555927059
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-06 18:36:43 -0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -18,7 +18,7 @@ module.exports = {
   .setThumbnail(member.user.displayAvatarURL)
   .setTimestamp()
   .setFooter(`» Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL)
-  .addField(`» ℹ Informações principais:`, `» Usuário: ${member.user.tag}\n» ID: ${member.user.id}\n» Status: ${status[member.user.presence.status]}\n» Jogando: ${member.user.presence.game ? member.user.presece.game : "O usuário não está jogando nada no momento."}\n» Conta criada em: ${moment(member.user.createdAt).format("LLL")}`)
+  .addField(`» ℹ Informações principais:`, `» Usuário: ${member.user.tag}\n» ID: ${member.user.id}\n» Status: ${status[member.user.presence.status]}\n» Jogando: ${member.user.presence.game ? member.user.presence.game : "O usuário não está jogando nada no momento."}\n» Conta criada em: ${moment(member.user.createdAt).format("LLL")}`)
   .addField(`» 📑 Informações no servidor:`, `» Apelido: ${member.user.nickname? member.user.nickname : "Sem apelido"}\n» Entrou em: ${moment(member.user.joinedAt).format("LLLL")}\n» Cargos: ${member.roles.size? member.roles.size : "Sem cargos."}\n» Administrador: ${administrator}`)
   .setAuthor(`» Informações do usuário: ${member.user.tag}`, member.user.displayAvatarURL)
   .setColor(member.hexColor)
