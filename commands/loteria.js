52e71b00ddee0fa1a7d569aafda52464632711cb
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-08 16:27:07 -0200

    Update loteria.js

--- commands/loteria.js
+++ commands/loteria.js
@@ -1,16 +1,14 @@
 module.exports = {
 	run: async function(bot, message, args) {
 
-	let random = function numbersRandom () {
-		return Math.floor(Math.random() * 50);
-	}
+	const random = Math.floor(Math.random() * 50);
 
 		message.channel.send(`Evento loteria iniciado! Um número aleátorio foi gerado entre 0 e 50. Para participar digite t.loteria "número". Boa sorte aos participantes!`);
-		const filter = msg => msg.content.includes(random());
+		const filter = msg => msg.content.includes(random;
 		const collected = await message.channel.awaitMessages(filter, { max: 1, time: 300000, erros: ["time"] }).then(collected => {
-		message.channel.send(`${collected.first().author} venceu o evento loteria. O número correto era **${random()}**.`)
+		message.channel.send(`${collected.first().author} venceu o evento loteria. O número correto era **${random}**.`)
 		}).catch(e => {
-			message.channel.send(`O evento loteria acabou, nenhum usuário venceu. O número correto era **${random()}**.`)
+			message.channel.send(`O evento loteria acabou, nenhum usuário venceu. O número correto era **${random}**.`)
 		});
 
 	},
