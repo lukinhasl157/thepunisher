822dd782c38944a0bef07e2e3bf1be143c2154b0
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-11 21:24:05 -0200

    Update chatlock.js

--- commands/chatlock.js
+++ commands/chatlock.js
@@ -9,9 +9,9 @@ if (!message.member.hasPermission("MANAGE_CHANNELS")) {
 }
   var everyone = message.guild.roles.find(r => r.name === "@everyone");
   message.channel.overwritePermissions(everyone, {
-     SEND_MESSAGES: false     
-})
-   message.channel.send(`» O canal ${message.channel} foi **DESATIVADO.** :lock: por **${message.author.username}**`);
+     SEND_MESSAGES: false
+});
+   message.channel.send(`» O canal ${message.channel} foi **DESATIVADO.** :lock: por **${message.author.username}**. Para reativar o chat utilize t.unlock`);
 
 } catch (e) {
 	message.channel.send(`**${message.author.usarname}** | Erro: Eu não tenho a permissão de **GERENCIAR_CANAIS**.`)
