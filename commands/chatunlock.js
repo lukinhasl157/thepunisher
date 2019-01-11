894138e35476a511327ebf6f08dc08f972a22752
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-11 21:24:14 -0200

    a

--- commands/chatunlock.js
+++ commands/chatunlock.js
@@ -10,7 +10,7 @@ if (!message.member.hasPermission("MANAGE_CHANNELS")) {
   var everyone = message.guild.roles.find(r => r.name === "@everyone");
   message.channel.overwritePermissions(everyone, {
      SEND_MESSAGES: true  
-})
+});
    message.channel.send(`» O canal ${message.channel} foi **REATIVADO.** :unlock: por **${message.author.username}**`);
 
 } catch (e) {
--- commands/clear.js
+++ commands/clear.js
@@ -8,13 +8,10 @@ module.exports = {
     const deleteCount = parseInt(args[0], 10);
     if (!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.channel.send(`**${message.author.username}** |  Por favor, forneça um número entre 2 e 100 para o número de mensagens a serem excluídas`);
-
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched);
-    message.channel.send(`**${message.author.username}** | O chat foi limpo com sucesso <a:sucessogif:499614074129350666>`).then(msg => msg.delete(10000));
-
+    message.channel.send(`**${message.author.username}** | O chat foi limpo com sucesso! <a:sucessogif:499614074129350666> | ${deleteCount} mensagens foram excluídas.`).then(msg => msg.delete(10000));
   },
-
     aliases: ["limpar"],
     category: "Moderação",
     description: "Deletar mensagens."
