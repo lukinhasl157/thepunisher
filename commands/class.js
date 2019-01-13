25b18222afdb169d3cdc3f5740c02432aec7d1ec
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-01-13 22:34:22 +0000

    Create class.js

---
+++ commands/class.js
@@ -0,0 +1,8 @@
+module.exports = {
+  run: (bot, message, args) => {
+    message.channel.send('VSF LUCAS: https://www.codecademy.com/courses/introduction-to-javascript/lessons/classes/')
+    },
+    aliases: ["lucaségay"],
+    category: "Utilidades",
+    description: "Dar a porra do link de class para o lucas."
+}
