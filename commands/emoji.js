78753f2d9708527c1df9dda8447ffbcc490a2c5d
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-14 19:07:36 -0200

    ffmpeg fdp

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,22 +1,16 @@
 module.exports = {
-    run: async function (bot, message, args) {
+    run: (bot, message, args) => {
 
     try {
-
-        let emoji = bot.emojis.find(e => e.name === `${args.join(" ")}`);
-
-    if (emoji) {
-      message.channel.send(emoji.url);
-
-    } else {
-
-      message.channel.send(`**${message.author.username}** | Erro: o emoji **${args.join(" ")}** não foi encontrado.`)
-    }
-
+    const emoji = bot.emojis.find(e => e.name === `${args.join(" ")}`);
+      if (emoji) {
+        message.channel.send(emoji.url);
+      } else {
+        message.channel.send(`**${message.author.username}** | Erro: o emoji **${args.join(" ")}** não foi encontrado.`)
+      }
     } catch(e) {
       message.channel.send(`**${message.author.username}**, deu merda quando tentei executar o comando **Emoji**, ${e}`)
     }
-
 },
     aliases: ["emote", "emojiinfo", "emoteinfo"],
     category: "Utilidades",
