7d20e2fff7bf14271023fc29e1c633356211420c
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-14 18:01:12 -0200

    Create teste.js

---
+++ commands/teste.js
@@ -0,0 +1,14 @@
+module.exports = {
+	run: (bot, messge, args) => {
+
+		if (message.author.id !== "289209067963154433") {
+			return message.channel.send("Comando em fase de teste.")
+		} else if (args.length === 0) {
+			return message.channel.send("Insira um link de musica.");
+		} else {
+			message.member.voiceChannel.join().then(m => {
+				m.playStream(args.join(" "))
+			});
+		}
+	}
+}
