5088f04b66daed52c61f9b44216100e3e5154078
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-14 09:32:02 -0200

    Update setnick.js

--- commands/setnick.js
+++ commands/setnick.js
@@ -8,7 +8,7 @@ module.exports = {
     		return message.channel.send(`**${message.author.username}** | Desculpe, você não tem permissão para executar este comando. Permissão necessária: **MANAGE_NICKNAMES**.`);
     	} else if (!member) {
     		return message.channel.send(`**${message.author.username}** | Por favor, insira o id ou mencione o usuário que deseja alterar o nick.`);
-    	} else if (!nick) {
+    	} else if (!nickName) {
     		return message.channel.send(`**${message.author.username}** | Por favor, insira um novo nickname para este usuário.`);
     	} else if (nickName.length > 32) {
     		return message.channel.send(`**${message.author.username}** | Desculpe, o limite de 32 caractéres do Discord foi atingido, tente um nick menor.`)
