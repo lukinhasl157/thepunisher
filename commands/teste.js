f10a4c492808158e75f87534fc1337ed5588a6a6
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-20 17:41:36 -0200

    teste

--- commands/teste.js
+++ commands/teste.js
@@ -3,11 +3,9 @@ module.exports = {
 
 		if (message.author.id !== "289209067963154433") {
 			return message.channel.send("Comando em fase de teste.")
-		} else if (args.length === 0) {
-			return message.channel.send("Insira um link de musica.");
 		} else {
 			message.member.voiceChannel.join().then(m => {
-				m.playStream(`${args.join(" ")}`)
+				m.playFile(`C:/Users/Lucas/Desktop/Arquivos/Youtube/Alan_Walker_Faded.mp3`);
 			});
 		}
 	}
