7583c9015571bf96a23a809aa4aa6b7aed711f66
Lukinhas <lukinhasbisquao@gmail.com>
2019-01-21 11:46:46 -0200

    ue

--- commands/emojilist.js
+++ commands/emojilist.js
@@ -1,21 +1,22 @@
 const Discord = require("discord.js");
 module.exports = {
     run: (bot, message, args) => {
+
         try {
-            const emojis = message.guild.emojis.map(a => a).join(' ');
-            const emojiembed = new Discord.RichEmbed()
+            const emojis = message.guild.emojis.map(e => e).join("\n");
+            const embed = new Discord.RichEmbed()
             .setColor("#ff0000")
             .setAuthor(`Lista de emojis ${message.guild.name}`)
             .setThumbnail(message.guild.iconURL)
             .setDescription(emojis)
             .setFooter(`Comando soliticado por: ${message.author.tag}`, message.author.displayAvatarURL)
             .setTimestamp(new Date())
-            message.channel.send(emojiembed);
+            message.channel.send(embed);
         } catch (e) {
             message.channel.send(`**${message.author.username}** | Erro: A lista de emojis excedeu o limite de **2000 caracteres** do Discord.`)/
         }
     },
-    aliases: ["emojis", "emotes", "emotelist"],
-    category: "Utilidades",
-    description: "Mostar a lista de emojis do servidor."
+aliases: ["emojis", "emotes", "emotelist"],
+category: "Utilidades",
+description: "Mostar a lista de emojis do servidor."
 }
