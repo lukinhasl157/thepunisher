421b55cef805c0ac7d358ea9face5195fe0ad4ef
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-01-23 16:09:35 -0200

    a

--- commands/chatlock.js
+++ commands/chatlock.js
@@ -1,23 +1,18 @@
-
 module.exports = {
-    run: (bot, message, args) => {
+  run: (bot, message, args) => {
 
-    	try {
-
-if (!message.member.hasPermission("MANAGE_CHANNELS")) {
-  return message.channel.send(`**${message.author.username}** | Desculpe, você não permissão para executar este comando! Permissão requirida: **MANAGE_CHANNELS**`);
-}
-  var everyone = message.guild.roles.find(r => r.name === "@everyone");
-  message.channel.overwritePermissions(everyone, {
-     SEND_MESSAGES: false
-});
-   message.channel.send(`» O canal ${message.channel} foi **DESATIVADO.** :lock: por **${message.author.username}**. Para reativar o chat utilize t.unlock`);
-
-} catch (e) {
-	message.channel.send(`**${message.author.usarname}** | Erro: Eu não tenho a permissão de **GERENCIAR_CANAIS**.`)
-}
-   
-},
+    const everyone = message.guild.roles.find(r => r.name === "@everyone");
+    if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) {
+      return message.channel.send(`» **${message.author.username}** | Desculpe, eu preciso da permissão **MANAGE_CHANNELS** para executar este comando.`);
+    } else if (!message.member.hasPermission("MANAGE_CHANNELS")) {
+      return message.channel.send(`**${message.author.username}** | Desculpe, você não permissão para executar este comando! Permissão requirida: **MANAGE_CHANNELS**`);
+    } else {
+      message.channel.overwritePermissions(everyone, {
+        SEND_MESSAGES: false
+      });
+      message.channel.send(`» O canal ${message.channel} foi **DESATIVADO.** :lock: por **${message.author.username}**. Para reativar o chat utilize t.unlock`);
+    }
+  },
     aliases: ["chlock", "lock"],
     category: "Moderação",
     description: "Desativar o chat."
--- commands/chatunlock.js
+++ commands/chatunlock.js
@@ -1,24 +1,19 @@
-
 module.exports = {
-    run: (bot, message, args) => {
+  run: (bot, message, args) => {
 
-    	try {
-
-if (!message.member.hasPermission("MANAGE_CHANNELS")) {
-  return message.channel.send(`**${message.author.username}** | Desculpe, você não permissão para executar este comando! Permissão requirida: **MANAGE_CHANNELS**`);
-}
-  var everyone = message.guild.roles.find(r => r.name === "@everyone");
-  message.channel.overwritePermissions(everyone, {
-     SEND_MESSAGES: true  
-});
-   message.channel.send(`» O canal ${message.channel} foi **REATIVADO.** :unlock: por **${message.author.username}**`);
-
-} catch (e) {
-	message.channel.send(`**${message.author.usarname}** | Erro: Eu não tenho a permissão de **GERENCIAR_CANAIS**.`);
-}
-   
-},
-  aliases: ["chunlock", "unlock"],
-  category: "Moderação",
-  description: "Reativar o chat."
+    const everyone = message.guild.roles.find(r => r.name === "@everyone");
+    if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) {
+      return message.channel.send(`» **${message.author.username}** | Desculpe, eu preciso da permissão **MANAGE_CHANNELS** para executar este comando.`);
+    } else if (!message.member.hasPermission("MANAGE_CHANNELS")) {
+      return message.channel.send(`**${message.author.username}** | Desculpe, você não permissão para executar este comando! Permissão requirida: **MANAGE_CHANNELS**`);
+    } else {
+      message.channel.overwritePermissions(everyone, {
+        SEND_MESSAGES: true
+      });
+      message.channel.send(`» O canal ${message.channel} foi **ATIVADO.** :unlock: por **${message.author.username}**.`);
+    }
+  },
+    aliases: ["chunlock", "unlock"],
+    category: "Moderação",
+    description: "Reativar o chat."
 }
