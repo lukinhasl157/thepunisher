f10fb8bf6ec1291155a35b917fb253aebf338e3a
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-01-26 02:58:45 -0200

    fazendo um comando de kick decente

--- commands/ban.js
+++ commands/ban.js
@@ -8,32 +8,32 @@ module.exports = {
             } else if (!reason) {
                 return message.channel.send(`» **${message.author.username}** | Por favor, insira um motivo para banir este usuário.`);
             } else {
-                let msg = await message.channel.send(`» **${message.author.username}** | Você tem certeza de banir o usuário ${member} pelo motivo: **${reason}** ? Se **SIM**, clique no emoji <:correto:505155063963058187> para bani-lo. Se **NÃO** clique no emoji <:negado:505155029636874250> para cancelar esta ação.`);
+                const msg = await message.channel.send(`» **${message.author.username}** | Você tem certeza de banir o usuário ${member} pelo motivo: **${reason}** ? Se **SIM**, clique no emoji <:correto:505155063963058187> para bani-lo. Se **NÃO** clique no emoji <:negado:505155029636874250> para cancelar esta ação.`);
                 await msg.react(":correto:505155063963058187");
                 await msg.react(":negado:505155029636874250");
 
                 const filter = (r, u) => r.me && u.id === message.author.id;
                 const collector = msg.createReactionCollector(filter, {max: 1, time: 60 * 1000 });
-                    collector.on("collect", r => {
+                    collector.on("collect", (r) => {
                         msg.delete();
                         switch (r._emoji.name) {
                             case "correto":
                                 if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
-                                    return message.channel.send(`» **${message.author.username}** | Desculpe, eu preciso da permissão **BAN_MEMBERS** para executar este comando.`);
+                                    return message.channel.send(`» **${message.author.username}** | Desculpe, eu preciso da permissão \`\`BAN_MEMBERS\`\` para executar este comando.`);
                                 } else if (!message.member.hasPermission("BAN_MEMBERS")) {
-                                    return message.channel.send(`» **${message.author.username}** | Desculpe, você não tem permissão para executar este comando! Permissão requirida: **BAN_MEMBERS**.`);
+                                    return message.channel.send(`» **${message.author.username}** | Desculpe, você não tem permissão para executar este comando! Permissão requirida: \`\`BAN_MEMBERS\`\`.`);
                                 } else if (message.guild.me.highestRole.position <= member.highestRole.position) {
                                     return message.channel.send(`» **${message.author.username}** | Desculpe, o meu cargo é menor ou igual ao usuário a ser banido.`);
                                 } else if (member.highestRole.position >= message.member.highestRole.position) {
                                     return message.channel.send(`» **${message.author.username}** | Desculpe, o cargo do usuário a ser banido é maior ou igual ao seu.`);
                                 } else {
-                                    member.send(`» **${member.user.username}** | Você foi banido por **${message.author.username}**. » Motivo: ${reason}.`);
+                                    member.send(`» **${member.user.username}** | Você foi banido por **${message.author.username}**. \n» Motivo: \`\`${reason}\`\`.`);
                                     member.ban(reason);
                                     message.channel.send(`» O usuário **${member.user.tag} ID:** \`\`${member.user.id}\`\` | Foi banido com sucesso. <:correto:505155063963058187>`);
                                 }
                             break;
                             case "negado":
-                                message.channel.send(`» A acão de banimento do usuário **${member.user.tag} ID:** \`\`${member.user.id}\`\` | Foi cancelada com sucesso. <:negado:505155029636874250>`);
+                                message.channel.send(`» A acão de banimento do usuário **${member.user.tag}** ID: \`\`${member.user.id}\`\` | Foi cancelada com sucesso. <:negado:505155029636874250>`);
                             break;
                         }
                     });
@@ -43,5 +43,3 @@ module.exports = {
     category: "Moderação",
     description: "Banir um usuário."
  }
-
-
--- commands/kick.js
+++ commands/kick.js
@@ -1,35 +1,45 @@
-  const Discord = require("discord.js");
-
-  module.exports = {
-    run: async function (bot, message, args) {
-
-      try {
-
-    if (!message.member.hasPermission("KICK_MEMBERS"))
-        return message.channel.send(`**${message.author.username}** | Desculpe, você não tem permissão para executar este comando! Permissão requirida: **KICK_MEMBERS**.`);
-      
-      let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if (!member)
-        return message.channel.send(new Discord.RichEmbed().setDescription(`${message.author} Por favor, mencione o membro que deseja kickar.`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setTimestamp().setColor("#ff0000"));
-
-    if (!member.kickable) 
-        return message.channel.send(new Discord.RichEmbed().setDescription(`<:cancel1:500150315304091649> Desculpe, você não tem as permissões necessárias para expulsar este usuário`).setColor("#ff0000"));
-  
-      let reason = args.join(' ');
-    if (!reason)
-      return message.channel.send(`**${message.author.username}** | Por favor, diga um motivo para expulsar este usuário.`);
-        
-      await member.kick(reason)
-        message.channel.send(new Discord.RichEmbed().setDescription(`O usuário ${member.user.tag} foi kickado  por ${message.author}`).setFooter(`Comando solicitado por: ${message.author.tag}`, message.author.displayAvatarURL).setThumbnail(member.user.avatarURL).addField(`Id do usuário:` , `» ${member.user.id}`).addField(`Motivo:` , `» ${reason}`).setTimestamp().setColor("#ff0000"));
+module.exports = {
+  run: async function (bot, message, args) {
     
-      } catch(e) {
-        message.channel.send(`**${message.author.usarname}** | Deu merda aqui: ${e}`)
-      }
+    const member = message.mentions.members.first() || message.guild.member.get(args[0]);
+    const reason = args.slice(1).join(" ");
+      if (!member) {
+        return message.channel.send(`» **${message.author.username}** | Por favor, insira o id ou mencione o usuário que deseja expulsar.`)
+      } else if (!reason) {
+        return message.channel.send(`» **${message.author.username}** | Por favor, insira um motivo para expulsar este usuário.`)
+      } else {
+        const msg = await message.channel.send(`» **${message.author.username}** | Você tem certeza de expulsar o usuário ${member} pelo motivo: \`\`${reason}\`\` ? Se **SIM**, clique no emoji <:correto:505155063963058187> para bani-lo. Se **NÃO** clique no emoji <:negado:505155029636874250> para cancelar esta ação.`)
+        await msg.react(":correto:505155063963058187");
+        await msg.react(":negado:505155029636874250");
 
-      },
-        aliases: ["expulsar", "chutar"],
-        category: "Moderação",
-        description: "Expulsar um usuário."
+        const filter = (r, u) => r.me && u.id === message.author.id;
+        const collector = msg.createReactionCollector(filter, { time: 60 * 1000, max: 1 });
+
+          collector.on("collect", (r) => {
+            msg.delete();
+            switch (r.emoji.id) {
+              case "505155063963058187":
+                if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
+                  return message.channel.send(`» **${message.author.username}** | Desculpe, eu preciso da permissão \`\`BAN_MEMBERS\`\` para executar este comando.`);
+                } else if (!message.member.hasPermission("KICK_MEMBERS")) {
+                  return message.channel.send(`» **${message.author.username}** | Desculpe, você não tem permissão para executar este comando! Permissão necessária: \`\`BAN_MEMBERS\`\`.`);
+                } else if (message.guild.me.highestRole.position <= member.highestRole.position) {
+                  return message.channel.send(`» **${message.author.username}** | Desculpe, o meu cargo é menor ou igual ao usuário a ser expulso.`);
+                } else if (member.highestRole.position >= message.member.highestRole.position) {
+                  return message.channel.send(`» **${message.author.username}** | Desculpe, o cargo do usuário a ser expulso é maior ou igual ao seu.`);
+                } else {
+                  member.kick(reason);
+                  member.send(`» **${member.user.username}** | Você foi expulso por **${message.author.username}**. \n» Motivo: \`\`${reason}\`\`.`);
+                  message.channel.send(`» O usuário **${member.user.tag} ID:** \`\`${member.user.id}\`\` | Foi expulso com sucesso. <:correto:505155063963058187>`);
+                }
+              break;
+              case "505155029636874250":
+                message.channel.send(`» A acão para expulsar o usuário **${member.user.tag}** ID: \`\`${member.user.id}\`\` | Foi cancelada com sucesso. <:negado:505155029636874250>`);
+              break;
+            }
+          });
       }
+  }
+}
     
     
