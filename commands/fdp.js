58ae4fdd6f9a72a68e109682a7e74ef1581e37f1
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-01-27 17:44:07 -0200

    Update fdp.js

--- commands/fdp.js
+++ commands/fdp.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports = {
 	run: (bot, message, args) => {
 
-		const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.guild.members.find(m => m.user.username.startsWith(args.join(" ")));
+		const member = message.mentions.members.first() || message.guild.members.get(args[0]);
 		if (!member) {
 			return message.channel.send(`» **${message.author.username}** | Por favor, insira o id, nome ou mencione o usuário que deseja xingar.`);
 		} else {
