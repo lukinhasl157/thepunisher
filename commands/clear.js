94bb6f20502a00a3b4ee35eea6d43aab470e5262
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-01-31 20:11:43 -0200

    ata

--- commands/clear.js
+++ commands/clear.js
@@ -10,7 +10,9 @@ module.exports = {
     } else if (!deleteCount || deleteCount < 2 || deleteCount > 100) {
       return message.channel.send(`**${message.author.username}** |  Por favor, forneça um número entre 2 e 100 para o número de mensagens a serem excluídas`);
     } else {
-      message.channel.bulkDelete(fetched);
+      message.channel.bulkDelete(fetched).then((msg) => {
+        msg.delete();
+      });
       message.channel.send(`**${message.author.username}** | O chat foi limpo com sucesso! <a:sucessogif:499614074129350666> | ${deleteCount} mensagens foram excluídas.`).then(msg => msg.delete(10000));
     }
   },
