74754f0a737d16c649eca8ed07214f9896fd13bf
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-02-05 05:11:11 -0200

    fodase

--- eventos/message.js
+++ eventos/message.js
@@ -37,5 +37,40 @@ module.exports.run = async function(message) {
                 msg.delete(60 * 1000);
             });
         }
+
+        if (message.guild.id !== "445077312589791253") {
+            return;
+        } else {
+            global.xp = "";
+            global.nextLevel = "";
+            const points = Math.floor(Math.random() * 7) + 8;
+    
+            database.ref(`Servidores/Levels/${message.guild.id}/${message.author.id}`)
+                .once("value").then(async function(snap) {
+                    if (snap.val == null) {
+                        database.ref(`Servidores/Levels/${message.guild.id}/${message.author.id}`)
+                            .set({
+                                xp: 0,
+                                level: 1
+                            });
+                    } else {
+                        xp = snap.val().xp + points;
+                        nextLevel = snap.val().level * 500;
+                        database.ref(`Servidores/Levels/${message.guild.id}/${message.author.id}`)
+                            .update({
+                                xp: xp
+                            });
+                        if (nextLevel <= xp) {
+                            nextLevel = snap.val().level + 1;
+                            database.ref(`Servidores/Levels/${message.guild.id}/${message.author.id}`)
+                                .update({
+                                    level: nextLevel
+                                });
+                            
+                            await message.channel.send(`Parabéns **${message.author.tag}** você subiu de nível!`);
+                        }
+                    }
+                });
+        }
     } 
 }
