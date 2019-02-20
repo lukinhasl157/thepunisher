5e196dd33e83799df344d051983aa47cb785d87f
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-02-20 09:05:35 -0300

    ata

--- commands/level.js
+++ commands/level.js
@@ -1,8 +1,10 @@
-module.exports = {
-    run: async function(database, message, ref) {
 
-        const memberRef = await database().ref(`${message.guild.id}/${message.author.id}`);
+module.exports = {
+    run: async function(database, message, ref, args) {
+
+        const memberRef = await database.ref(`${message.guild.id}/${message.author.id}`);
         const data = await ref.once('value');
+        const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
 
         if (data.val() === null) {
             ref.set({
@@ -11,7 +13,11 @@ module.exports = {
             });
             await message.channel.send("Nível atual: 1");
         } else {
-            message.channel.send(`Nível atual: ${memberRef().level + 1}`);
+            memberRef.data.then(async function(DBlevel, DBxp) {
+                const level = DBlevel.val().level;
+                const xp = DBxp().xp;
+                await message.channel.send(`Level atual: ${level}, xp: ${xp}`);
+            });
         }
     }
 }
