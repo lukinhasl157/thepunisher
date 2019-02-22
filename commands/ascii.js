b4c0accc6756dcf77f395f423b3600838ab0618f
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-02-22 00:03:38 -0300

    fodase

--- commands/ascii.js
+++ commands/ascii.js
@@ -3,9 +3,9 @@ module.exports = {
     run: async (bot, message, args) => {
 
         if (args.length === 0) {
-            return message.channel.send(`» **${message.author.username}** | Os argumentos não podem passar de  **15** caracteres.`)
-        } else if (args.length >= 15) {
-            return message.channel.send(`» **${message.author.username}** | Os argumentos não podem passar de  **15** caracteres.`)
+            return message.channel.send(`» **${message.author.username}** | Insira uma mensagem (15 caracteres)`);
+        } else if (args.length > 15) {
+            return message.channel.send(`» **${message.author.username}** | Os argumentos não podem passar de  **15** caracteres.`);
         } else {
             ascii.font(args.join(" "), 'Doom', function (rendered) {
                 rendered = rendered.trimRight()
