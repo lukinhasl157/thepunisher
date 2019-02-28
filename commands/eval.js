b1e7aa749f3aae310b0c613c862b822a9cbc86d0
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-02-28 07:59:31 -0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -8,20 +8,20 @@ module.exports = {
     } else if (args.length === 0) {
         return message.channel.send(`**${message.author.username}** | Insira um código.`);
     } else {
-        const code = args.join(' ').replace(/^```(js|javascript ?\n)?|```$/g, '')
-        const value = (l, c) => `\`\`\`${l}\n${String(c).slice(0, 1000) + (c.length >= 1000 ? '...' : '')}\n\`\`\``.replace(process.env.token, () => '*'.repeat(process.env.token.length))
-        const embed = new Discord.RichEmbed()
+      const code = args.join(' ').replace(/^```(js|javascript ?\n)?|```$/g, '')
+      const value = (l, c) => `\`\`\`${l}\n${String(c).slice(0, 1000) + (c.length >= 1000 ? '...' : '')}\n\`\`\``.replace(process.env.token, () => '*'.repeat(process.env.token.length))
+      const embed = new Discord.RichEmbed()
           .setColor('#36393F')
-        try {
-          const resultEval = eval(code);
-          let toEval = typeof resultEval === 'string' ? resultEval : inspect(resultEval, { depth: 1 })
+      try {
+        const resultEval = eval(code);
+        let toEval = typeof resultEval === 'string' ? resultEval : inspect(resultEval, { depth: 1 })
           embed.addField('Result', value('js', toEval))
           embed.addField('Type', value('css', typeof resultEval))
-        } catch (error) {
-          embed.addField('Error', value('js', error))
-        } finally {
-          message.channel.send(embed);
-        }
+      } catch (error) {
+        embed.addField('Error', value('js', error))
+      } finally {
+        message.channel.send(embed);
+      }
     }
   },
   aliases: ["evaluate", "code"],
