60c1bacb15db02a6fc126c531a0ceb132273e8e8
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-02-28 08:32:11 -0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -8,12 +8,12 @@ module.exports = {
     } else if (args.length === 0) {
         return message.channel.send(`**${message.author.username}** | Insira um código.`);
     } else {
-      const code = args.join(' ').replace(/^```(js|javascript ?\n)?|```$/g, '')
-      const value = (l, c) => `\`\`\`${l}\n${String(c).slice(0, 1000) + (c.length >= 1000 ? '...' : '')}\n\`\`\``.replace(process.env.token, () => '*'.repeat(process.env.token.length))
+      let code = args.join(' ').replace(/^```(js|javascript ?\n)?|```$/g, '')
+      let value = (l, c) => `\`\`\`${l}\n${String(c).slice(0, 1000) + (c.length >= 1000 ? '...' : '')}\n\`\`\``.replace(process.env.token, () => '*'.repeat(process.env.token.length))
       let embed = new Discord.RichEmbed()
         .setColor('#36393F')
       try {
-        const resultEval = eval(code);
+        let resultEval = eval(code);
         let toEval = typeof resultEval === 'string' ? resultEval : inspect(resultEval, { depth: 1 });
           embed.addField('Resultado', value('js', toEval))
           embed.addField("Código", value('js', resultEval))
