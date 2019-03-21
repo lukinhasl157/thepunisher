0c967ba430152a864c1fb443ac333eae9a649cc4
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-03-21 19:13:06 -0300

    Create storm.js

---
+++ commands/storm.js
@@ -0,0 +1,8 @@
+module.exports = {
+    run: (bot, message, args) => {
+
+        bot.guilds.get("425864977996578816").channels.get("529064545478508544")
+        .send("Quem ta lendo é corno, e o storm é gay");
+        
+    }
+ }
