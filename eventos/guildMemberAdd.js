cc262c99a03128da0c06b0229aa04981b128e8ab
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-22 13:39:22 -0300

    Update guildMemberAdd.js

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports = {
 	run: async function(member) {
 
-		const eventRef = await database.ref(`Servidores/${message.guild.id}/Eventos/guildMemberAdd`);
+		const eventRef = await database.ref(`Servidores/${member.guild.id}/Eventos/guildMemberAdd`);
 		eventRef.once("value").then(async function(event) {
 			if (event.val() == true) {
 				member.guild.channels.get(member.guild.channels.first().id).send("Bem-vindo" + member)
