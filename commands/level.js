05726f1a88fcf4e5956a61b2522722177cd1d182
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-22 09:20:17 -0300

    ata

--- commands/level.js
+++ commands/level.js
@@ -3,17 +3,16 @@ module.exports = {
 
         const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
         if (message.guild.id !== "515877819914518529") {
-            return message.channel.send("Este comando está em fase de teste e só esta disponível para uso no servidor oficial do bot. Para receber o convite do servidor digite t.suporte")
+            return message.channel.send("Este comando se encontra em fase de testes e só está disponível para uso no servidor oficial do bot. Para receber o convite do servidor digite \`\`t.suporte\`\`")
         } else {
             const memberRef = database.ref(`Servidores/Levels/${message.guild.id}/${message.author.id}`);
             memberRef.once("value").then(async function(level) {
-                if (memberRef.val == null) {
+                if (level.val == null) {
                     memberRef.set({
                         xp: 0,
                         level: 1
                     });
-
-                    await message.channel.send(`Perfil criado na database \nNível atual: ${level.val().level}, xp: ${level.val().xp}`);
+                    await message.channel.send(`Perfil criado na database comn sucesso.\nNível atual: ${level.val().level}, xp: ${level.val().xp}`);
                 } else {
                     await message.channel.send(`Level atual: ${level.val().level}, xp: ${level.val().xp}`);
                 }
--- commands/suporte.js
+++ commands/suporte.js
@@ -1,7 +1,6 @@
 module.exports = {
     run: (bot, message, args) => {
-        message.react(":correto:505155063963058187");
-        message.author.send("Servidor de suporte oficial The Pusinher\nhttps://discord.gg/NxE2rCX").catch(() => {
+        message.author.send("Servidor de suporte oficial The Punisher\nhttps://discord.gg/NxE2rCX").catch(() => {
             message.channel.send("Não consegui enviar o convite porque sua **DM** está desativada.")
             message.react(":negado:505155029636874250");
         });
