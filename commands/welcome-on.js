9b8a4dd482e17b61b7e7b925b2ec6f48a83672c8
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-22 14:12:52 -0300

    buceta

--- commands/welcome-on.js
+++ commands/welcome-on.js
@@ -2,11 +2,15 @@ module.exports = {
     run: async function(bot, message, args, database) {
         const eventRef = await database.ref(`Servidores/${message.guild.id}/Eventos/guildMemberAdd`);
 
-        eventRef.once("value").then(async function() {
-            eventRef.update({
-                guildMemberAdd: true
-            });
-            await message.channel.send("O evento \`\`bem-vindo\`\` foi ativado com sucesso.");
+        eventRef.once("value").then(async function(event) {
+            if (event.val() == true) {
+                message.channel.send("Desculpe, o evento bem-vindo já está ativado.")
+            } else {
+                eventRef.update({
+                    guildMemberAdd: true
+                });
+                await message.channel.send("O evento \`\`bem-vindo\`\` foi ativado com sucesso.");
+            }
         });
     }
 }
