9c8ffcfd6154941643fd40d9c5f7f86ea09d0186
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-22 09:47:54 -0300

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -12,7 +12,7 @@ module.exports = {
                         xp: 0,
                         level: 1
                     });
-                    await message.channel.send(`Perfil criado na database comn sucesso.\nNível atual: ${db.val().level}, xp: ${db.val().xp}`);
+                    await message.channel.send(`Perfil criado na database comn sucesso.\nNível atual: ${memberRef.val().level}, xp: ${memberRef.val().xp}`);
                 } else {
                     await message.channel.send(`Level atual: ${db.val().level}, xp: ${db.val().xp}`);
                 }
