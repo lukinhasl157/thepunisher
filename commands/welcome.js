0d423ac538f3028c7d17f775f922cc6b9270825b
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-22 12:28:18 -0300

    async e meu pau

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,5 +1,5 @@
 module.exports = {
-    run: (bot, message, args, database) => {
+    run: async function(bot, message, args, database) {
         const eventRef = await database.ref(`Servidores/${message.guild.id}/Eventos/guildMemberAdd`);
 
         eventRef.once("value").then(async function(event) {
