d56ba6cba2fc3275d889eb1020106a89e0f32e34
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-22 13:16:36 -0300

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,17 +1,22 @@
 module.exports = {
     run: async function(bot, message, args, database) {
         const eventRef = await database.ref(`Servidores/${message.guild.id}/Eventos`);
+        const eventRefi = await database.ref(`Servidores/${message.guild.id}/Eventos/guildMemberAdd`);
 
         eventRef.once("value").then(async function(event) {
             if (event.val() == null) {
                 eventRef.set({
                     guildMemberAdd: true
                 });
-            } else if (event.val() == true) {
+            }
+        });
+
+        eventRef.once("value").then(async function(event) {
+            if (event.val() == true) {
                 message.channel.send("O evento de bem-vindo está ativado.");
             } else {
                 if (event.val() == false) {
-                    message.channel.send("O evento de bem-vindo está desativado.");
+                    message.channel.send("O evendo de bem-vindo está desativado.")
                 }
             }
         });
