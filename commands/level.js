3757cb5d23cc789b1ba5c55514716ecf8bbfa659
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-22 09:26:43 -0300

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -7,12 +7,12 @@ module.exports = {
         } else {
             const memberRef = database.ref(`Servidores/Levels/${message.guild.id}/${message.author.id}`);
             memberRef.once("value").then(async function(level) {
-                if (level.val() == null) {
+                if (memberRef.val() == null) {
                     memberRef.set({
                         xp: 0,
                         level: 1
                     });
-                    await message.channel.send(`Perfil criado na database comn sucesso.\nNível atual: ${level.val().level}, xp: ${level.val().xp}`);
+                    await message.channel.send(`Perfil criado na database comn sucesso.\nNível atual: ${memberRef.val().level}, xp: ${memberRef.val().xp}`);
                 } else {
                     await message.channel.send(`Level atual: ${level.val().level}, xp: ${level.val().xp}`);
                 }
