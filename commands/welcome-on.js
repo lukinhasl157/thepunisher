f103651a0af1fcec10b68f63d3d4ff778044c051
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-22 13:58:47 -0300

    ata

--- commands/welcome-on.js
+++ commands/welcome-on.js
@@ -2,10 +2,8 @@ module.exports = {
     run: async function(bot, message, args, database) {
         const eventRef = await database.ref(`Servidores/${message.guild.id}/Eventos/guildMemberAdd`);
 
-        eventRef.once("value").then(async function() {
-            eventRef.update({
-                guildMemberAdd: true
-            });
+        eventRef.once("value").update({
+            guildMemberAdd: true
         });
     }
 }
