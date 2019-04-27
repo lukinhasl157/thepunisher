d1ec151c7159f88eecf83463e48cd88d598df9f2
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-27 09:31:26 -0300

    krl

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -1,17 +1,5 @@
-const firebase = require("firebase");
-const database = firebase.database();
 module.exports = {
 	run: async function(member) {
-
-		const eventRef = await database.ref(`Servidores/${member.guild.id}/Eventos/guildMemberAdd`);
-		eventRef.once("value").then(async function(event) {
-			if (event.val() == true) {
-				member.guild.channels.get(member.guild.channels.first().id).send("Bem-vindo" + member)
-			} else {
-				if (event.val() == false) {
-					return;
-				}
-			}
-		});
+		member.guild.channels.get(member.guild.channels.first().id).send("Bem-vindo" + member)
 	}
 }
--- eventos/guildMemberRemove.js
+++ eventos/guildMemberRemove.js
@@ -1,37 +1,5 @@
-const Discord = require("discord.js");
 module.exports = {
 	run: async function(member) {
-
-		try {
-			let channel = member.guild.channels.find(ch => ch.name === "🎉saiu");
-			let category = member.guild.channels.find(ch => ch.name === "👾ENTRADA/SAIDA")
-			const embed = new Discord.RichEmbed()
-      			.setColor("#3fdb20")
-      			.setThumbnail(member.user.avatarURL)
-      			.setDescription(`${member}, saiu do servidor.`)
-      			.addField('Atualmente temos:', member.guild.memberCount)
-      			.setTimestamp(new Date())
-      			.setFooter(member.guild.name, member.guild.iconURL)
-    		if (!member.guild.me.hasPermission("MANAGE_CHANNELS")) {
-    			return;
-			} else if (!category && !channel) {
-				category = await member.guild.createChannel("👾ENTRADA/SAIDA", "category");
-				channel = await member.guild.createChannel("🎉saiu", "text" [{
-					id: member.guild.id,
-					deny: ["SEND_MESSAGES"],
-					allow: ["ADD_REACTIONS", "VIEW_CHANNEL"]
-				}]);
-				channel.setParent(category.id);
-				await channel.send(embed);
-			} else {
-				if (channel && category) {
-					channel.send(embed);
-				}
-			}
-		} catch(e) {
-			const channel = this.channels.find(ch => ch.name === "❌logs-de-erros-the-punisher");
-			console.log(e);
-			channel.send(`Ocorreu um erro no evento **guildMemberAdd** | Servidor ${member.guild.name}. Erro: ${e}`);
-		}	
+		member.guild.channels.get(member.guild.channels.first().id).send("Bem-vindo" + member)
 	}
 }
