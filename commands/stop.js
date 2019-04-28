91e5e4a8104574eb821073e7867e67607accd833
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-27 21:37:34 -0300

    await meu pau

--- commands/stop.js
+++ commands/stop.js
@@ -1,5 +1,5 @@
 module.exports = {
-    run: (bot, message, args) => {
+    run: async function (bot, message, args) {
         message.member.voiceChannel.leave();
         await message.channel.send("Saindo do canal" + message.member.voiceChannel.name);
     }
