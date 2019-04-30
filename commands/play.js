9586dcd6d46ef7fb9ecc00434d445f494017d889
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-04-30 11:04:59 -0300

    a

--- commands/play.js
+++ commands/play.js
@@ -1,11 +1,9 @@
+const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 module.exports = {
     run: async function (bot, message, args) {
 
         const validator = await ytdl.validateURL(args.join(" "));
-        const info = await ytdl.getInfo(args.join(" "));
-        const connection = await message.member.voiceChannel.join();
-        const dispatcher = await connection.playStream(ytdl(args.join(" "), { filter: 'audioonly' }));
 
         if (!message.member.voiceChannel) {
             return message.channel.send("Por-favor, entre em um canal de voz primeiro!");
@@ -16,15 +14,23 @@ module.exports = {
         } else if (!validator) {
             return message.channel.send("Coloque uma url **valida** do Youtube!");
         } else {
+           message.member.voiceChannel.join().then(async function(connection) {
+            const stream = await ytdl(args.join(" "), {filter: 'audioonly'});
+            console.log(stream);
+            let dispatcher = connection.playStream(stream);
+           }).catch((e) => {
+               message.channel.send("Deu merda aqui:" + e)
+            });
+            const info = await ytdl.getInfo(args.join(" "));
             const embed = new Discord.RichEmbed()
-                .setDescription(`:Labfm: Tocando: **${info.title}** no canal **${message.member.voiceChannel.name}**`)
+                .setDescription(`Tocando: **${info.title}** no canal **${message.member.voiceChannel.name}**`)
                 .setColor('#ff4040')
             message.channel.send(embed);
             dispatcher.on('end', async () => {
                 await message.member.voiceChannel.leave();
                 const embed2 = new Discord.RichEmbed()
-                    .setDescription(`:musicStopped: A Música terminou, **saindo de ${message.guild.me.voiceChannel.name}**...`)
-                    .setColor('#ff4040')
+                    .setDescription(` A Música terminou, **saindo do canal ${message.guild.me.voiceChannel.name}**...`)
+                    .setColor("RANDOM")
                 await message.channel.send(embed2);
             });
         }
