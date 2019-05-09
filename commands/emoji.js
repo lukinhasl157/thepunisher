582c4d69b750a2ae73a0b586d04874cb12302b98
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-05-09 17:53:50 -0300

    Update emoji.js

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js");
 module.exports = {
     run: (bot, message, args) => {
-    const emoji = bot.emojis.find((e) => e.name === `${args.join(" ")}`).replace(":", "");
 
-    message.channel.send(new Discord.RichEmbed()
-          .setImage(emoji.url)
+      function sendEmoji () {
+        const emojiSlice = args[0].slice(-19, -1) || bot.emojis.find((e) => e.name === `${args[0]}`);
+        message.channel.send(new Discord.RichEmbed()
+          .setImage(`https://cdn.discordapp.com/emojis/${emojiSlice}.png?v=1`)
           .setColor("RANDOM")
         );
+      }
+      sendEmoji();
 },
     aliases: ["emote", "emojiinfo", "emoteinfo"],
     category: "Utilidades",
