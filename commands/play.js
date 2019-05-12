649ab86b884da4db2b69f8b9fcc47fa10679dbae
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-05-12 17:30:10 -0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const ytdl = require("ytdl-core-discord");
+const ytdl = require("ytdl-core");
+const ytdl
 module.exports = {
     run: async function (bot, message, args) {
 
