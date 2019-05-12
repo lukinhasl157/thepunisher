2f9ab1bde4ffcf2fec8cc2bc45484fcf35ac3e86
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-05-12 18:33:53 -0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -8,16 +8,30 @@ module.exports = {
         } else if (args.length === 0) {
             return message.channel.send("Insira uma URL do youtube!");
         } else {
-            message.member.voiceChannel.join().then(async function(connection) {
-                const stream = connection.playOpusStream(await ytdl(args.join(" ")));
-                getInfo(args.join(" ")).then((info) => {
-                    message.channel.send(`Tocando a música \`\`${info.items[0].title}\`\` no canal \`\`${message.member.voiceChannel.name}\`\`...`)
-                    stream.on('end', async () => {
-                        await message.member.voiceChannel.leave();
-                        await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``)
+            if (new URL(args.join(" "))) {
+                message.member.voiceChannel.join().then(async function(connection) {
+                    const stream = connection.playOpusStream(await ytdl(args.join(" ")));
+                    getInfo(args.join(" ")).then((info) => {
+                        message.channel.send(`Tocando a música \`\`${info.items[0].title}\`\` no canal \`\`${message.member.voiceChannel.name}\`\`...`);
+                        stream.on('end', async () => {
+                            await message.member.voiceChannel.leave();
+                            await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``);
+                        });
                     });
                 });
-            });
+            } else {
+                message.member.voiceChannel.join().then(async function(connection) {
+                    getInfo(args.join(" ")).then(async function(search) {
+                        const result = search.items[0].url;
+                        const stream2 = connection.playOpusStream(await ytdl(result));
+                        message.channel.send(`Tocando a música \`\`${info.items[0].title}\`\` no canal \`\`${message.member.voiceChannel.name}\`\`...`);
+                        stream2.on('end', async () => {
+                            await message.member.voiceChannel.leave();
+                            await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``);
+                        });
+                    });
+                });
+            }
         }
     }
 }
