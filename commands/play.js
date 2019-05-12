c1ba5b34e191afb8c02e3747b5441c7804bd93c9
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-05-12 19:09:37 -0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -8,9 +8,7 @@ module.exports = {
         } else if (args.length === 0) {
             return message.channel.send("Insira uma URL do youtube!");
         } else {
-            if (!new URL(args.join(" "))) {
-                return message.channel.send("URL inválido");
-            } else if (new URL(args.join(" "))) {
+            if (new URL(args.join(" "))) {
                 message.member.voiceChannel.join().then(async function(connection) {
                     const stream = connection.playOpusStream(await ytdl(args.join(" ")));
                     getInfo(args.join(" ")).then((info) => {
@@ -22,17 +20,19 @@ module.exports = {
                     });
                 });
             } else {
-                message.member.voiceChannel.join().then(async function(connection) {
-                    getInfo(args.join(" ")).then(async function(search) {
-                        const result = await search.items[0].url;
-                        const stream2 = connection.playOpusStream(await ytdl(result));
-                        message.channel.send(`Tocando a música \`\`${search.items[0].title}\`\` no canal \`\`${message.member.voiceChannel.name}\`\`...`);
-                        stream2.on('end', async () => {
-                            await message.member.voiceChannel.leave();
-                            await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``);
+                if (!new URL(args.join(" "))) {
+                    message.member.voiceChannel.join().then(async function(connection) {
+                        getInfo(args.join(" ")).then(async function(search) {
+                            const result = await search.items[0].url;
+                            const stream2 = connection.playOpusStream(await ytdl(result));
+                            message.channel.send(`Tocando a música \`\`${search.items[0].title}\`\` no canal \`\`${message.member.voiceChannel.name}\`\`...`);
+                            stream2.on('end', async () => {
+                                await message.member.voiceChannel.leave();
+                                await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``);
+                            });
                         });
                     });
-                });
+                }
             }
         }
     }
