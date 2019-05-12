4bc23bf0f7d3cd268e27ca43a7f601046e90e830
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-05-12 17:48:12 -0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -12,16 +12,10 @@ module.exports = {
             message.member.voiceChannel.join().then(async function(connection) {
                 const stream = connection.playOpusStream(await ytdl(args.join(" ")));
                 const info = getInfo(args.join(" "))
-                const embed = new Discord.RichEmbed()
-                    .setDescription(`Tocando a música \`\`${info.items[0].title}\`\` no canal \`\`${message.member.voiceChannel.name}\`\`...`)
-                    .setColor('#ff4040')
-                message.channel.send(embed);  
+                message.channel.send(`Tocando a música \`\`${info.items[0].title}\`\` no canal \`\`${message.member.voiceChannel.name}\`\`...`)
                 stream.on('end', async () => {
                     await message.member.voiceChannel.leave();
-                    const embed2 = new Discord.RichEmbed()
-                        .setDescription(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``)
-                        .setColor("RANDOM")
-                    await message.channel.send(embed2);
+                    await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``)
                 });
             });
         }
