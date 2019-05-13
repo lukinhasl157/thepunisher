947b7566ca2fe1d88215c268afcec6c74ce3f1b7
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-05-13 11:17:12 +0100

    arrumando a parada do getVideo

--- commands/play.js
+++ commands/play.js
@@ -12,15 +12,15 @@ module.exports = {
         } else if (args.length === 0) {
             return message.channel.send("Insira uma URL do youtube!");
         } else {
-            if (checkUrl(args.join(" "))) {
+            if (checkUrl(args[0])) {
                 message.member.voiceChannel.join().then(async function(connection) {
-                    const stream = connection.playOpusStream(await ytdl(args.join(" ")));
-                    youtube.getVideo(args.join(" ")).then(async function(video) {
+                    const stream = connection.playOpusStream(await ytdl(args[0]));
+                    youtube.getVideo(args[0]).then(async function(video) {
                         if (video) {
                             message.channel.send(new Discord.RichEmbed()
-                                .addField("Nome da música:", video[0].title)
+                                .addField("Nome da música:", video.title)
                                 .addField("Canal:", video[0].channel.title)
-                                .addField("Descrição do vídeo:", video[0].description)
+                                .addField("Descrição do vídeo:", video.description)
                                 .setImage(video[0].thumbnails.high.url)
                                 .setTimestamp(new Date())
                                 .setFooter(`Musica solicitada por ${message.author.tag}`, message.author.displayAvatarURL)
