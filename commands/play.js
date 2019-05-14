827d3c17c024f1f6e700db4f4b8413c64ed1fa06
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-05-14 18:25:03 -0300

    eita porra

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,7 @@
 const ytdl = require("ytdl-core-discord");
 const Youtube = require("simple-youtube-api");
 const youtube = new Youtube(process.env.google_api_key);
+const { fetchVideoInfo } = require("youtube-info");
 const Discord = require("discord.js");
 module.exports = {
     run: async function (bot, message, args) {
@@ -16,47 +17,65 @@ module.exports = {
                 message.member.voiceChannel.join().then(async function(connection) {
                     const stream = connection.playOpusStream(await ytdl(args[0]));
                     youtube.getVideo(args[0]).then(async function(video) {
-                        if (video) {
-                            message.channel.send(new Discord.RichEmbed()
-                                .addField("Nome da música:", video.title)
-                                .addField("Nome do canal:", video.channel.title)
-                                .addField("Descrição do vídeo:", video.description)
-                                .setImage(video.thumbnails.medium.url)
-                                .setTimestamp(new Date())
-                                .setFooter(`Musica solicitada por ${message.author.tag}`, message.author.displayAvatarURL)
-                                .setColor("RANDOM")
-                            );
-                            stream.on('end', async () => {
-                                await message.member.voiceChannel.leave();
-                                await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``);
-                            });
-                        } else {
-                            return message.channel.send("A URL que você inseriu está inválida.")
-                        }
+                        fetchVideoInfo(video.url).then(async function(videoInfo) {
+                            if (video) {
+                                message.channel.send(new Discord.RichEmbed()
+                                    .addField("Nome da música:", videoInfo.title, true)
+                                    .addField("Nome do canal:", videoInfo.owner, true)
+                                    .addField("Descrição do vídeo:", videoInfo.description)
+                                    .addField("Duração da Música", videoInfo.duration)
+                                    .addField("Visualizações", videoInfo.views, true)
+                                    .addField("Comentários", videoInfo.commentCount, true)
+                                    .addField("Likes", videoInfo.likeCount, true)
+                                    .addField("Dislikes", videoInfo.dislikeCount, true)
+                                    .addField("Gênero", videoInfo.genre)
+                                    .setImage(videoInfo.thumbnailUrl)
+                                    .setThumbnail(videoInfo.channelThumbnailUrl)
+                                    .setTimestamp(new Date())
+                                    .setFooter(`Musica solicitada por ${message.author.tag}`, message.author.displayAvatarURL)
+                                    .setColor("RANDOM")
+                                );
+                                stream.on('end', async () => {
+                                    await message.member.voiceChannel.leave();
+                                    await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``);
+                                });
+                            } else {
+                                return message.channel.send("A URL que você inseriu está inválida.")
+                            }
+                        });
                     });
                 });
             } else {
                 message.member.voiceChannel.join().then(async function(connection) {
                     youtube.searchVideos(args[0]).then(async function(search) {
-                        if (search) {
-                            const stream2 = connection.playOpusStream(await ytdl(search[0].url));
-                            message.channel.send(new Discord.RichEmbed()
-                                .addField("Nome da música:", search[0].title)
-                                .addField("Nome do canal:", search[0].channel.title)
-                                .addField("Descrição do vídeo:", search[0].description)
-                                .setImage(search[0].thumbnails.medium.url)
-                                .setTimestamp(new Date())
-                                .setFooter(`Musica solicitada por ${message.author.tag}`, message.author.displayAvatarURL)
-                                .setColor("RANDOM")
-                            );
-                            stream2.on('end', async () => {
-                                await message.member.voiceChannel.leave();
-                                await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``);
-                            });
-                        } else {
-                            return message.channel.send(`Nenhuma música foi encontrada com o argumento \`\`${args.join(" ")}\`\``);
-                        }
-                    });
+                        fetchVideoInfo(search[0].url).then(async function(videoInfo) {
+                            if (search) {
+                                const stream2 = connection.playOpusStream(await ytdl(search[0].url));
+                                message.channel.send(new Discord.RichEmbed()
+                                    .addField("Nome da música:", videoInfo.title, true)
+                                    .addField("Nome do canal:", videoInfo.owner, true)
+                                    .addField("Descrição do vídeo:", videoInfo.description)
+                                    .addField("Duração da Música", videoInfo.duration)
+                                    .addField("Visualizações", videoInfo.views, true)
+                                    .addField("Comentários", videoInfo.commentCount, true)
+                                    .addField("Likes", videoInfo.likeCount, true)
+                                    .addField("Dislikes", videoInfo.dislikeCount, true)
+                                    .addField("Gênero", videoInfo.genre)
+                                    .setImage(videoInfo.thumbnailUrl)
+                                    .setThumbnail(videoInfo.channelThumbnailUrl)
+                                    .setTimestamp(new Date())
+                                    .setFooter(`Musica solicitada por ${message.author.tag}`, message.author.displayAvatarURL)
+                                    .setColor("RANDOM")
+                                );
+                                stream2.on('end', async () => {
+                                    await message.member.voiceChannel.leave();
+                                    await message.channel.send(`A Música terminou, saindo do canal \`\`${message.guild.me.voiceChannel.name}\`\``);
+                                });
+                            } else {
+                                return message.channel.send(`Nenhuma música foi encontrada com o argumento \`\`${args.join(" ")}\`\``);
+                            }
+                        });
+                    }); 
                 });
             }
         }
