b9db332f9015a6dc8d236a25125c4ef12b62ab3a
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-06-01 05:55:59 -0300

    ata

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -2,7 +2,7 @@ const musics = require("../utils/queue.js");
 module.exports = {
     run: async function(_, message, args) {
 
-        if (!message.member.voiceChannel || message.member.voiceChannel.id !== message.guild.me.voiceChannel.id) {
+        if (!message.member.voiceChannel || message.member.voiceChannel !== message.guild.me.voiceChannel) {
             return message.channel.send("Você precisa estar no mesmo canal de voz que eu para poder ver música")
         } else if (musics.get(message.guild.id) == undefined || musics.get(message.guild.id).songs.length == 0) {
             return message.channel.send("Não há nenhuma música tocando no momento.")
