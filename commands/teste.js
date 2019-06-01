fc04fc6bffffa0165b909bd8b8717b1852ec90c2
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-06-01 03:52:38 -0300

    ata

--- commands/teste.js
+++ commands/teste.js
@@ -5,6 +5,8 @@ const fetchVideoInfo = require("youtube-info");
 const Discord = require("discord.js");
 const REGEX_URL = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/i
 const checkUrl = (url) => REGEX_URL.test(url)
+const queue = require("../utils/queue");
+const serverQueue = serverQueue;
 
 module.exports = {
     run: async function (_, message, args, queue) {
@@ -19,14 +21,6 @@ module.exports = {
         } else if (args.length === 0) {
             return message.channel.send("Insira uma URL do youtube, ou pesquisa uma musica pelo nome");
         } else {
-            const queueConstruct = {
-                textChannel: message.channel,
-                voiceChannel: message.member.voiceChannel,
-                connection: null,
-                songs: [],
-                volume: 5,
-                playing: true,
-            };
             if (checkUrl(args[0])) {
                 message.member.voiceChannel.join().then(async function(connection) {
                     youtube.getVideo(args[0]).then(async function(video) {
@@ -67,45 +61,58 @@ module.exports = {
                         const filter = (r, u) => r.me && u.id === message.author.id;
                         const collector = msg.createReactionCollector(filter, { max: 1, time: 60 * 1000 });
 
+                        async function play() {
+                            message.member.voiceChannel.join().then(async function(connection) {
+                                fetchVideoInfo(search[0].id).then(async function(videoInfo) {
+                                    if (!serverQueue || serverQueue == undefined) {
+                                        const queueConstruct = {
+                                            textChannel: message.channel,
+                                            voiceChannel: message.member.voiceChannel,
+                                            connection: null,
+                                            songs: [],
+                                            volume: 5,
+                                            playing: true,
+                                        };
+                                        queue.set(message.guild.id, queueConstruct);
+                                        serverQueue.songs.push(videoInfo.url);
+
+                                        connection.playOpusStream(await ytdl(serverQueue.songs[0])).on("end", async (reason) => {
+                                            if (reason !== null) {
+                                                serverQueue.songs.shift();
+                                            } else {
+                                                serverQueue.voiceChannel.leave();
+                                                queue.delete(message.guild.id);
+                                                await message.channel.send(`A música acabou, saindo do canal \`\`${serverQueue.textChannel.name}...\`\``);
+                                            }
+                                        });
+                                        embed.addField("📀Música", `[${videoInfo.title}](${videoInfo.url})`)
+                                        embed.addField("🎧Canal", `[${videoInfo.owner}](https://youtube.com/channel/${videoInfo.channelId})`)
+                                        embed.addField("📈Visualizações", videoInfo.views, true)
+                                        embed.addField("📝Comentários", videoInfo.commentCount, true)
+                                        embed.addField("👍Likes", videoInfo.likeCount, true)
+                                        embed.addField("👎Dislikes", videoInfo.dislikeCount, true)
+                                        embed.addField("⏰Duração da música", videoInfo.duration, true)
+                                        embed.addField("🎭Gênero", videoInfo.genre, true)
+                                        embed.setThumbnail(videoInfo.thumbnailUrl)
+                                        embed.setTimestamp(new Date())
+                                        embed.setFooter(`Musica solicitada por ${message.author.tag}`, message.author.displayAvatarURL)
+                                        embed.setColor("#e83127")
+                                        serverQueue.textChannel.send(embed);
+                                    } else {
+                                        if (serverQueue.songs.length > 0) {
+                                            serverQueue.songs.push(videoInfo.url);
+                                            message.channel.send(`A música [${videoInfo.title}](${videoInfo.url}) foi adicionada a fila com sucesso!`);
+                                        }
+                                    }
+                                });
+                            });
+                        }
+
                         collector.on("collect", async (r) => {
                             msg.delete();
                             switch (r.emoji.name) {
                                 case "1⃣":
-                                    message.member.voiceChannel.join().then(async function(connection) {
-                                        fetchVideoInfo(search[0].id).then(async function(videoInfo) {
-                                            if (!queue.get(message.guild.id) || queue.get(message.guild.id) == undefined) {
-                                                queue.set(message.guild.id, queueConstruct);
-                                                queue.get(message.guild.id).songs.push(videoInfo.url);
-                                                const stream2 = connection.playOpusStream(await ytdl(queue.get(message.guild.id).songs[0]));
-                                                embed.addField("📀Música", `[${videoInfo.title}](${videoInfo.url})`)
-                                                embed.addField("🎧Canal", `[${videoInfo.owner}](https://youtube.com/channel/${videoInfo.channelId})`)
-                                                embed.addField("📈Visualizações", videoInfo.views, true)
-                                                embed.addField("📝Comentários", videoInfo.commentCount, true)
-                                                embed.addField("👍Likes", videoInfo.likeCount, true)
-                                                embed.addField("👎Dislikes", videoInfo.dislikeCount, true)
-                                                embed.addField("⏰Duração da música", videoInfo.duration, true)
-                                                embed.addField("🎭Gênero", videoInfo.genre, true)
-                                                embed.setThumbnail(videoInfo.thumbnailUrl)
-                                                embed.setTimestamp(new Date())
-                                                embed.setFooter(`Musica solicitada por ${message.author.tag}`, message.author.displayAvatarURL)
-                                                embed.setColor("#e83127")
-                                                message.channel.send(embed);
-                                                stream2.on("end", async () => {
-                                                    queue.get(message.guild.id).songs.shift();
-                                                });
-                                            } else {
-                                                if (queue.get(message.guild.id) || queue.get(message.guild.id).songs.length > 0) {
-                                                    queue.get(message.guild.id).songs.push(videoInfo.url);
-                                                    const streamQueue = connection.playOpusStream(await ytdl(queue.get(message.guild.id).songs[0]));
-                                                    message.channel.send("A música foi adicionada a fila com sucesso!");
-                                                    streamQueue.on("end", async () => {
-                                                        queue.get(message.guild.id).textChannel.leave();
-                                                        await message.channel.send(`A música acabou, saindo do canal \`\`${queue.get(message.guild.id).textChannel.name}...\`\``);
-                                                    });
-                                                }
-                                            }
-                                        });
-                                    });
+                                    play(search[0]);
                                 break;
                                 case "2⃣":
                                     message.member.voiceChannel.join().then(async function(connection) {
