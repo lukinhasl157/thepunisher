a562154da076a44cc1f9cc670b4fee0f70320a03
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-06-01 04:57:16 -0300

    ata

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,10 +1,10 @@
+const musics = require("../utils/queue.js");
 module.exports = {
-    run: async function(_, message, args, queue) {
-        const serverQueue = queue.get(message.guild.id);
-        if (!serverQueue) {
+    run: async function(_, message, args) {
+        if (musics.get(message.guild.id).songs.length == 0) {
             return message.channel.send("Não há nenhuma música tocando no momento.")
         } else {
-            message.channel.send(serverQueue.songs[0]);
+            message.channel.send(musics.get(message.guild.id).songs[0]);
         }
     },
     aliases: ["np", "tocando"],
--- commands/queue.js
+++ commands/queue.js
@@ -1,10 +1,11 @@
+const musics = require("../utils/queue.js");
 module.exports = {
-    run: async function(_, message, args, queue) {
-        if (queue.get(message.guild.id).songs.length == 0) {
+    run: async function(_, message, args) {
+        if (musics.get(message.guild.id).songs.length == 0) {
             return message.channel.send("Não há nenhuma música tocando no momento.");
         } else {
-            if (queue.get(message.guild.id).songs.length >= 1) {
-                message.channel.send(queue.get(message.guild.id).songs);
+            if (musics.get(message.guild.id).songs.length > 0) {
+                message.channel.send(musics.get(message.guild.id).songs);
             }
         }
     }
