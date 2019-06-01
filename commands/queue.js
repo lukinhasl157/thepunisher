0cae03e3748a3eda614d8fa04b364b358a513475
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-06-01 05:43:25 -0300

    ta quase porra

--- commands/queue.js
+++ commands/queue.js
@@ -1,10 +1,11 @@
 const musics = require("../utils/queue.js");
 module.exports = {
     run: async function(_, message, args) {
-        if (message.member.voiceChannel.id !== message.guild.me.voiceChannel.id) {
-            return message.channel.send("Você precisa estar no mesmo canal de voz que eu para poder ver a fila de músicas.")
+
+        if (!message.member.voiceChannel || message.member.voiceChannel.id !== message.guild.me.voiceChannel.id) {
+            return message.channel.send("Você precisa estar no mesmo canal de voz que eu para poder ver a fila de músicas.");
         } else if (musics.get(message.guild.id) == undefined || musics.get(message.guild.id).songs.length == 0) {
-            return message.channel.send("Não há nenhuma música tocando no momento.")
+            return message.channel.send("Não há nenhuma música tocando no momento.");
         } else {
             return message.channel.send(musics.get(message.guild.id).songs);
         }
