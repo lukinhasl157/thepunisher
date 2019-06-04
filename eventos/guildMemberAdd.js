48fbaff01f1a0548e6904440f40fe9ec209f72e1
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-06-03 22:48:51 -0300

    captcha monstro

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -1,26 +1,73 @@
 module.exports = {
 	run: async function(member) {
 
-		const role = member.guild.roles.find((r) => r.name === "captchaTeste");
+		member.guild.createRole({
+			name: "Captcha Teste",
+			color: "RED",
+			permissions: [
+				"ADD_REACTIONS",
+				"READ_MESSAGE_HISTORY",
+				"VIEW_CHANNEL"
+			]
+		});
 		member.addRole(role);
 
-		const channel = member.guild.channels.find((r) => r.name === "captchateste");
-		channel.send("teste").then(async function(msg) {
-			await msg.react("😜");
+		member.guild.createChannel("captcha", "category");
+		member.guild.createChannel(`Captcha ${member.id}`, "text");
 
-			const filter = (r, u) => r.me && u.id === member.id;
-			const collector = msg.createReactionCollector(filter, { max: 1 });
+		const category = member.guild.channels.find((c) => c.name === "captcha");
+		const channel = member.guild.channels.find((ch) => ch.name === `Captcha ${member.id}`);
+		channel.setParent(category.id);
 
-			collector.on("collect", async (r) => {
-				msg.delete();
-				switch (r.emoji.name) {
-					case "😜":
-						const roleVerified = member.guild.roles.find((r) => r.name === "Verificado");
-						member.removeRole(role);
-						member.addRoler(roleVerified);
-						channel.send("Verificado com sucesso!");
-				}
+		const role = member.guild.roles.find((r) => r.name === "Captcha Teste");
+		channel.overwritePermissions(role, {
+			READ_MESSAGE_HISTORY: true,
+			VIEW_CHANNEL: true,
+			SEND_MESSAGES: false,
+			ADD_REACTIONS: true
+		});
+
+		const msg = channel.send("Teste");
+		msg.react("😜");
+		const filter = (r, u) => r.emoji.name === "😜" && u.id === member.id;
+		const collector = msg.createReactionCollector(filter, { max: 1 });
+
+		collector.on("collect", async (r) => {
+			member.guild.createRole({
+				name: "Verificado",
+				color: "GREEN",
+				permissions: [
+					"ADD_REACTIONS",
+					"READ_MESSAGE_HISTORY",
+					"VIEW_CHANNEL",
+					"SEND_MESSAGES",
+					"EMBED_LINKS",
+					"ATTACH_FILES",
+					"EXTERNAL_EMOJIS",
+					"CONNECT",
+					"SPEAK",
+					"CHANGE_NICKNAME"
+				]
 			});
+
+			const roleVerified = member.guild.roles.find((r) => r.name === "Verificado");
+			member.guild.channels.forEach(async (channel) => {
+				await channel.overwritePermissions(roleVerified, {
+					ADD_REACTIONS: true,
+					READ_MESSAGE_HISTORY: true,
+					VIEW_CHANNEL: true,
+					SEND_MESSAGES: true,
+					EMBED_LINKS: true,
+					ATTACH_FILES: true,
+					EXTERNAL_EMOJIS: true,
+					CONNECT: true,
+					SPEAK: true,
+				});
+			});
+
+			member.addRole(roleVerified);
+			category.delete();
+			member.send("Você foi verificado com sucesso! Agora você pode interagir no servidor.");		
 		});
 	}
 }
