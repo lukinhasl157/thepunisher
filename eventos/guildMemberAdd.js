3569745d4cfa67ac0a65afdede47fbbf4b11513d
lukinhasl157 <44874930+lukinhasl157@users.noreply.github.com>
2019-06-03 21:07:57 -0300

    teste

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -1,4 +1,22 @@
 module.exports = {
 	run: async function(member) {
+
+		const role = member.guild.roles.find((r) => r.name === "captchaTeste");
+		member.addRole(role);
+
+		const channel = member.guild.channels.find((r) => r.name === "captchaTeste");
+		channel.send("teste").then(async function(msg) {
+			await msg.react("😜");
+		});
+
+		const filter = (r, u) => r.me && u.id === member.id;
+		const collector = msg.createReactionCollector(filter, { max: 1 });
+
+		collector.on("collect", async (r) => {
+			switch (r.emoji.name) {
+				case "😜":
+					member.removeRole(role);
+			}
+		});
 	}
 }
