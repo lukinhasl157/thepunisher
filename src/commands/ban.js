0623c3602b3c0de72fc0d9a9672c8586c32cd1b5
Yslan Ramos <yslan3000@gmail.com>
2020-11-12 02:34:04 -0300

    fix(ban): adicionando os emojis em sequencias

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,57 +1,66 @@
-'use strict';
 module.exports = {
+  // eslint-disable-next-line consistent-return
   run: async ({ message, args }) => {
     // Gif ban the punisher https://media.giphy.com/media/1Xe14KOTgtL86EGBXU/giphy.gif
-    const member = message.mentions.members.first() || message.guild.members.get(args[0]),
-      reason = args.slice(1).join(' ');
+    const member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    const reason = args.slice(1).join(' ');
 
     if (!member) {
-      return message.reply(`Por favor, insira o id ou mencione o usuário que deseja banir.`);
-    } else if (!reason) {
-      return message.reply(`Por favor, insira um motivo para banir este usuário.`);
-    } else {
-      const msg = await message.reply(`Você tem certeza de banir o usuário ${member} pelo motivo: \`${reason}\` ? Se **SIM**, clique no emoji <:correto:604266535262879746> para bani-lo. Se **NÃO** clique no emoji <:negado:604266617379225620> para cancelar esta ação.`),
-        emojis = ['604266535262879746', '604266617379225620'];
+      return message.reply('Por favor, insira o id ou mencione o usuário que deseja banir.');
+    }
 
-      for (const i in emojis) {
-        await msg.react(emojis[i]);
+    if (!reason) {
+      return message.reply('Por favor, insira um motivo para banir este usuário.');
+    }
+
+    const msg = await message.reply(`Você tem certeza de banir o usuário ${member} pelo motivo: \`${reason}\` ? Se **SIM**, clique no emoji <:correto:604266535262879746> para bani-lo. Se **NÃO** clique no emoji <:negado:604266617379225620> para cancelar esta ação.`);
+    const ACCEPT_EMOJI_ID = '604266535262879746';
+    const RECUSE_EMOJI_ID = '604266617379225620';
+
+    (async () => {
+      await msg.react(ACCEPT_EMOJI_ID);
+      await msg.react(RECUSE_EMOJI_ID);
+    })();
+
+    const filter = (r, u) => r.me && u.equals(message.author);
+    const collector = msg.createReactionCollector(filter, { max: 1, time: 60 * 1000 });
+
+    collector.on('collect', async (r) => {
+      msg.delete();
+      if (r.emoji.id === ACCEPT_EMOJI_ID) {
+        if (!message.guild.me.permissions.has('BAN_MEMBERS')) {
+          return message.reply('Desculpe, eu preciso da permissão `BAN_MEMBERS` para executar este comando.');
+        }
+
+        if (!message.member.permissions.has('BAN_MEMBERS')) {
+          return message.reply('Desculpe, você precisa da permissão `BAN_MEMBERS` para executar este comando.');
+        }
+
+        if (message.member.user.equals(message.guild.owner)) {
+          await member.send(`» **${member.user.username}** | Você foi banido por **${message.author.username}**. \n» Motivo: \`${reason}\`.`).catch(() => null);
+          await member.ban(reason);
+          return message.reply(`» O usuário **${member.user.tag} ID:** \`${member.user.id}\` | Foi banido com sucesso. <:correto:604266535262879746>`);
+        }
+
+        if (member.user.equals(message.guild.ownerID)) {
+          return message.reply('Desculpe, você não pode banir o dono do servidor.');
+        }
+
+        if (message.guild.me.roles.highest.position <= member.roles.highest.position) {
+          return message.reply('Desculpe, o meu cargo é menor ou igual ao usuário a ser banido.');
+        }
+
+        if (member.roles.highest.position >= message.member.roles.highest.position) {
+          return message.reply('Desculpe, o cargo do usuário a ser banido é maior ou igual ao seu.');
+        }
+
+        await member.send(`» **${member.user.username}** | Você foi banido por **${message.author.username}**. \n» Motivo: \`${reason}\`.`).catch(() => null);
+        await member.ban(reason);
+        return message.reply(`» O usuário **${member.user.tag} ID:** \`${member.user.id}\` | Foi banido com sucesso. <:correto:604266535262879746>`);
       }
 
-      const filter = (r, u) => r.me && u.equals(message.author),
-        collector = msg.createReactionCollector(filter, { max: 1, time: 60 * 1000 });
-
-      collector.on('collect', async (r) => {
-        msg.delete();
-        switch (r.emoji.id) {
-          case emojis[0]: {
-            if (!message.guild.me.permissions.has('BAN_MEMBERS')) {
-              return message.reply(`Desculpe, eu preciso da permissão \`BAN_MEMBERS\` para executar este comando.`);
-            } else if (!message.member.permissions.has('BAN_MEMBERS')) {
-              return message.reply(`Desculpe, você precisa da permissão \`BAN_MEMBERS\` para executar este comando.`);
-            } else if (message.member.user.equals(message.guild.owner)) {
-              await member.send(`» **${member.user.username}** | Você foi banido por **${message.author.username}**. \n» Motivo: \`${reason}\`.`).catch(() => null);
-              await member.ban(reason);
-              return message.reply(`» O usuário **${member.user.tag} ID:** \`${member.user.id}\` | Foi banido com sucesso. <:correto:604266535262879746>`);
-            } else if (member.user.equals(message.guild.ownerID)) {
-              return message.reply(`Desculpe, você não pode banir o dono do servidor.`);
-            } else if (message.guild.me.roles.highest.position <= member.roles.highest.position) {
-              return message.reply(`Desculpe, o meu cargo é menor ou igual ao usuário a ser banido.`);
-            } else if (member.roles.highest.position >= message.member.roles.highest.position) {
-              return message.reply(`Desculpe, o cargo do usuário a ser banido é maior ou igual ao seu.`);
-            } else {
-              await member.send(`» **${member.user.username}** | Você foi banido por **${message.author.username}**. \n» Motivo: \`${reason}\`.`).catch(() => null);
-              await member.ban(reason);
-              message.reply(`» O usuário **${member.user.tag} ID:** \`${member.user.id}\` | Foi banido com sucesso. <:correto:604266535262879746>`);
-            }
-            break;
-          }
-          case emojis[1]: {
-            message.reply(`» A acão de banimento do usuário **${member.user.tag}** ID: \`${member.user.id}\` | Foi cancelada. <:negado:604266617379225620>`);
-            break;
-          }
-        }
-      });
-    }
+      return message.reply(`» A acão de banimento do usuário **${member.user.tag}** ID: \`${member.user.id}\` | Foi cancelada. <:negado:604266617379225620>`);
+    });
   },
   name: 'ban',
   aliases: ['banir', 'punir'],
