3843808563255c3f206aadafe14bf3c68da787d3
Yslan Ramos <yslan3000@gmail.com>
2020-11-12 02:21:47 -0300

    mostra erro quando não consegui criar o emote

--- src/commands/addemoji.js
+++ src/commands/addemoji.js
@@ -1,17 +1,22 @@
-'use strict';
 module.exports = {
   run: async ({ message, args }) => {
-    const url = args.shift(),
-      emojiName = args.join('-');
+    const url = args.shift();
+    const emojiName = args.join('-');
 
     if (!url) {
       return message.channel.send(`**${message.author.username}** | Por favor, insira o link do emoji que deseja adicionar.`);
-    } else if (!emojiName) {
-      return message.channel.send(`**${message.author.username}** | Por favor, insira um nome para este emoji.`);
-    } else {
-      const emoji = await message.guild.emojis.create(url, emojiName).catch(console.error);
+    }
 
-      message.channel.send(`Emoji ${emoji.toString()} foi adicionado com sucesso!`);
+    if (!emojiName) {
+      return message.channel.send(`**${message.author.username}** | Por favor, insira um nome para este emoji.`);
+    }
+
+    try {
+      const emoji = await message.guild.emojis.create(url, emojiName);
+      return message.channel.send(`Emoji ${emoji.toString()} foi adicionado com sucesso!`);
+    } catch (error) {
+      console.error(error);
+      return message.channel.send('Não consegui adicionar...');
     }
   },
   name: 'addemoji',
