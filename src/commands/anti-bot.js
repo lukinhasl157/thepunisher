6b5de2c89fd65d79a06425473a2d17b97b2abc9d
Yslan Ramos <yslan3000@gmail.com>
2020-11-12 02:26:52 -0300

    fix(anti-bot, autorole): mostra erro quando opção é invalida

--- src/commands/anti-bot.js
+++ src/commands/anti-bot.js
@@ -1,38 +1,45 @@
-'use strict';
 module.exports = {
   run: ({ message, server, args }) => {
-    const status = server.events.get('guildMemberAdd').antiBot.status,
-      type = args[0];
+    const { status } = server.events.get('guildMemberAdd').antiBot;
+    const type = args[0];
 
     if (!message.member.hasPermission('ADMINISTRATOR')) {
       return message.channel.send(`» **${message.author.username}** | Desculpe, você precisa da permissão \`ADMINISTRATOR\` para executar este comando.`);
-    } else if (args.length === 0) {
+    }
+
+    if (args.length === 0) {
       return message.channel.send(`O modo **ANTI-BOT** está \`${status ? 'ativado' : 'desativado'}\``);
-    } else if (message.author.id !== message.guild.ownerID) {
+    }
+
+    if (message.author.id !== message.guild.ownerID) {
       return message.channel.send('Este comando só pode ser executado pelo dono do servidor.');
-    } else {
-      switch (type) {
-        case 'on': {
-          if (status) {
-            return message.channel.send('O modo **ANTI-BOT** já está \`ativado\`');
-          } else {
-            message.channel.send('O modo **ANTI-BOT** foi \`ativado\` com sucesso.');
-            server.events.get('guildMemberAdd').antiBot.status = true;
-            server.save();
-          }
-          break;
+    }
+
+    switch (type) {
+      case 'on': {
+        if (status) {
+          return message.channel.send('O modo **ANTI-BOT** já está `ativado`');
         }
-        case 'off': {
-          if (!status) {
-            return message.channel.send('O modo **ANTI-BOT** já está \`desativado\`');
-          } else {
-            message.channel.send('O modo **ANTI-BOT** foi \`desativado\` com sucesso.');
-            server.events.get('guildMemberAdd').antiBot.status = false;
-            server.save();
-          }
-          break;
-        }
+
+        server.events.get('guildMemberAdd').antiBot.status = true;
+        server.save();
+
+        return message.channel.send('O modo **ANTI-BOT** foi `ativado` com sucesso.');
       }
+
+      case 'off': {
+        if (!status) {
+          return message.channel.send('O modo **ANTI-BOT** já está `desativado`');
+        }
+
+        server.events.get('guildMemberAdd').antiBot.status = false;
+        server.save();
+
+        return message.channel.send('O modo **ANTI-BOT** foi `desativado` com sucesso.');
+      }
+
+      default:
+        return message.channel.send('opção invalida!');
     }
   },
   name: 'anti-bot',
--- src/commands/autorole.js
+++ src/commands/autorole.js
@@ -1,101 +1,99 @@
-'use strict';
+/* eslint-disable default-case */
 const { MessageEmbed } = require('discord.js');
+
+const isId = (id) => /(?:<@&?)?([0-9]{16,19})(?:>)?/.test(id);
+
 module.exports = {
   run: ({ message, args, server }) => {
-    const { roles, status } = server.events.get('guildMemberAdd').autoRole,
-      type = args[0];
+    const { roles, status } = server.events.get('guildMemberAdd').autoRole;
+    const type = args[0];
 
     if (!message.member.hasPermission('ADMINISTRATOR')) {
       return message.channel.send(`» **${message.author.username}** | Desculpe, você não tem permissão para executar este comando! Permissão requirida: \`ADMINISTRADOR\`.`);
-    } else if (args.length === 0) {
+    }
+
+    if (args.length === 0) {
       return message.channel.send(`O autoRole está ${status ? 'ativado' : 'desativado'}`);
-    } else {
-      switch (type) {
-        case 'on': {
-          if (status) {
-            return message.channel.send('O autoRole já está ativado.');
-          } else {
-            message.channel.send('O autoRole foi ativado com sucesso!');
-            server.events.get('guildMemberAdd').autoRole.status = true;
-            server.save();
-          }
-          break;
-        }
-        case 'off': {
-          if (!status) {
-            return message.channel.send('O autoRole já está desativado.');
-          } else {
-            message.channel.send('O autoRole foi desativado com sucesso!');
-            server.events.get('guildMemberAdd').autoRole.status = false;
-            server.save();
-          }
-          break;
-        }
-        case 'list': {
-          if (roles.length === 0) {
-            return message.channel.send(`Nenhum cargo foi adicionado a lista, caso queira adicionar algum cargo a lista digite: \`${server.prefix}autorole add @cargo\``);
-          } else {
-            message.channel.send(new MessageEmbed()
-              .addField('Lista de cargos do autoRole:', roles.map((i) => `<@&${i}>`).join(', '))
-              .setColor('#ff0000')
-              .setThumbnail(message.guild.iconURL())
-            );
-          }
-          break;
-        }
-        case 'remove': {
-          const role = message.mentions.roles.first();
-          if (!role) {
-            return message.reply('Por favor, mencione o cago que deseja remover da minha lista.');
-          } else if (args.length > 2) {
-            return message.reply('Por favor, remova um cargo de cada vez.');
-          } else if (!roles.find((i) => i === role.id)) {
-            return message.reply('Não encontrei este cargo na minha lista.');
-          } else {
-            const index = roles.indexOf(role.id);
-            roles.splice(index, 1);
-            message.reply(`O cargo ${role} foi removido da lista com sucesso! Para ver a lista de cargos digite: \`${server.prefix}autorole list\``);
-            server.save();
-          }
-          break;
-        }
-        case 'reset': {
-          message.channel.send('A lista de cargos foi resetada com sucesso!');
-          server.events.get('guildMemberAdd').autoRole.roles = [];
-          server.save();
-          break;
-        }
-        case 'add': {
-          let rolesList = [];
-          for (const i in args) {
-            if (/(?:<@&?)?([0-9]{16,19})(?:>)?/.test(args[i])) {
-              const id = args[i].replace(/(<@&)?(>)?/g, '');
-              if (message.guild.roles.get(id) && message.guild.id !== id) rolesList.push(id);
-            }
-          }
+    }
 
-          const nonAddedRoles = [];
-          for (const i in rolesList) {
-            if (roles.includes(rolesList[i])) nonAddedRoles.push(rolesList[i]);
-          }
-          if (nonAddedRoles.length) {
-            message.channel.send(`Os cargos seguites não foram adicionados pois já estavam incluídos: ${nonAddedRoles.map((r) => `\`${(message.guild.roles.get(r) && message.guild.roles.get(r).name) || 'desconhecido'}\``)}`);
-            for (const i in nonAddedRoles) {
-              const index = rolesList.indexOf(nonAddedRoles[i]);
-              rolesList.splice(index, 1);
-            }
-          }
-
-          if (!roles.length) {
-            return message.channel.send('Insira o ID ou mencione um ou mais cargo.');
-          } else {
-            message.channel.send(`Os cargos seguintes foram adicionados: ${rolesList.map((r) => `\`${message.guild.roles.get(r).name}\``)}. Para ver todos os cargos digite \`${server.prefix}autorole list\``);
-            roles.push(rolesList);
-            server.save();
-          }
-          break;
+    switch (type) {
+      case 'on': {
+        if (status) {
+          return message.channel.send('O autoRole já está ativado.');
         }
+        server.events.get('guildMemberAdd').autoRole.status = true;
+        server.save();
+        return message.channel.send('O autoRole foi ativado com sucesso!');
       }
+
+      case 'off': {
+        if (!status) {
+          return message.channel.send('O autoRole já está desativado.');
+        }
+        server.events.get('guildMemberAdd').autoRole.status = false;
+        server.save();
+        return message.channel.send('O autoRole foi desativado com sucesso!');
+      }
+
+      case 'list': {
+        if (roles.length === 0) {
+          return message.channel.send(`Nenhum cargo foi adicionado a lista, caso queira adicionar algum cargo a lista digite: \`${server.prefix}autorole add @cargo\``);
+        }
+
+        return message.channel.send(new MessageEmbed()
+          .addField('Lista de cargos do autoRole:', roles.map((i) => `<@&${i}>`).join(', '))
+          .setColor('#ff0000')
+          .setThumbnail(message.guild.iconURL()));
+      }
+
+      case 'remove': {
+        const role = message.mentions.roles.first();
+        if (!role) {
+          return message.reply('Por favor, mencione o cago que deseja remover da minha lista.');
+        }
+        if (args.length > 2) {
+          return message.reply('Por favor, remova um cargo de cada vez.');
+        }
+        if (!roles.find((i) => i === role.id)) {
+          return message.reply('Não encontrei este cargo na minha lista.');
+        }
+        const index = roles.indexOf(role.id);
+        if (index !== -1) {
+          roles.splice(index, 1);
+          server.save();
+        }
+        return message.reply(`O cargo ${role} foi removido da lista com sucesso! Para ver a lista de cargos digite: \`${server.prefix}autorole list\``);
+      }
+
+      case 'reset': {
+        server.events.get('guildMemberAdd').autoRole.roles = [];
+        server.save();
+        return message.channel.send('A lista de cargos foi resetada com sucesso!');
+      }
+
+      case 'add': {
+        const [rolesList, nonAddedRoles] = args.reduce((p, v) => {
+          if (isId(v) && message.guild.roles.has(v) && message.guild.id !== v) {
+            p[roles.includes(v) ? 1 : 0].push(v);
+          }
+          return p;
+        }, [[], []]);
+
+        if (nonAddedRoles.length) {
+          message.channel.send(`Os cargos seguites não foram adicionados pois já estavam incluídos: ${nonAddedRoles.map((r) => `\`${(message.guild.roles.get(r) && message.guild.roles.get(r).name) || 'desconhecido'}\``)}`);
+        }
+
+        if (!roles.length) {
+          return message.channel.send('Insira o ID ou mencione um ou mais cargo.');
+        }
+
+        roles.push(rolesList);
+        server.save();
+        return message.channel.send(`Os cargos seguintes foram adicionados: ${rolesList.map((r) => `\`${message.guild.roles.get(r).name}\``)}. Para ver todos os cargos digite \`${server.prefix}autorole list\``);
+      }
+
+      default:
+        return message.channel.send('Opção invalida!');
     }
   },
   name: 'autorole',
